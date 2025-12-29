fbb9fb603e3c293c6e8278f91fee124d862e3b3c
Seonggyu-Min <minsunggu705@gmail.com>
2025-12-30 02:01:56 +0900

    feat: Refresh 기능 추가

---
+++ Assets/Editor/CiRefresh.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class CiRefresh
+{
+    public static void Refresh()
+    {
+        AssetDatabase.Refresh();
+        AssetDatabase.SaveAssets();
+        Debug.Log("[CI] Refresh 완료");
+    }
+}
