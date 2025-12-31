c670820f507685ecefe17591727606ce95927fe5
Seonggyu-Min <minsunggu705@gmail.com>
2025-12-31 12:07:58 +0900

    feat: 데이터베이스 관리용 유틸 클래스 추가
    - DB 접근 Key 상수 관리용 DatabaseKeys 추가
    - 조립된 DB 접근 도메인 관리용 DBRoutes 추가
    - DB key 조립용 DBPathMaker 추가

---
+++ Assets/Scripts/Utils/Database/DBRoutes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    /// <summary>
+    /// DB 경로 접근 시 오타 방지 및 공통화를 위해 사용하는 클래스입니다.
+    /// </summary>
+    public static class DBRoutes
+    {
+        // ----- User Data -----
+        public static string Users(string uid) => DBPathMaker.Join(DatabaseKeys.users, uid);
+        public static string Nickname(string uid) => DBPathMaker.Join(DatabaseKeys.users, uid, DatabaseKeys.nickname);
+
+
+       
+    }
+}
---
+++ Assets/Scripts/Utils/Database/DatabaseKeys.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    /// <summary>
+    /// 데이터베이스 노드 접근 시 사용하는 키들을 오타 방지를 위해 상수로 관리하는 클래스입니다.
+    /// </summary>
+    public static class DatabaseKeys
+    {
+        // -- User Data Keys --
+        public const string users = "users";
+        public const string nickname = "nickname";
+        public const string email = "email";
+
+        // -- Friend Data Keys --
+
+    }
+}
