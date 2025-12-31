8470c5b7a22c2089a18e095a8ac35c864a88fa75
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-01 02:27:02 +0900

    feat: 파이어베이스 접근용 매니저 및 초기화 기능 추가

---
+++ Assets/Scripts/Managers/FirebaseManager.cs
@@ -0,0 +1,50 @@
+﻿using Cysharp.Threading.Tasks;
+using Firebase.Auth;
+using Firebase.Database;
+using System;
+using System.Threading;
+
+
+public class FirebaseManager : Singleton<FirebaseManager>
+{
+    #region Fields And Properties
+
+    private FirebaseAuth _auth;
+    private FirebaseDatabase _database;
+
+    public bool IsReady => _auth != null && _database != null;
+
+    public event Action OnFirebaseReady;
+    public event Action OnFirebaseLoadFailed;
+
+    public FirebaseAuth Auth => _auth ??= FirebaseAuth.DefaultInstance;
+    public FirebaseDatabase Database => _database ??= FirebaseDatabase.DefaultInstance;
+
+    #endregion
+
+
+    #region Public Methods
+
+    public async UniTask Initialize(CancellationToken ct)
+    {
+        var status = await Firebase.FirebaseApp.CheckAndFixDependenciesAsync()
+            .AsUniTask()
+            .AttachExternalCancellation(ct);
+
+        if (status != Firebase.DependencyStatus.Available)
+        {
+            throw new Exception($"Firebase 연결 실패: {status}");
+        }
+
+        _auth = FirebaseAuth.DefaultInstance;
+        _database = FirebaseDatabase.DefaultInstance;
+        _database.GoOnline();
+    }
+
+    public void GoOfflineRTDB()
+    {
+        if (_database != null) _database.GoOffline();
+    }
+
+    #endregion
+}
