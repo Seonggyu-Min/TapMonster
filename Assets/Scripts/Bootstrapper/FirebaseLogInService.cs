1fcab4ba45a7c1eb3b4aaf2589648f069ebfe731
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-01 02:26:45 +0900

    feat: 파이어베이스 로그인 클래스 추가

---
+++ Assets/Scripts/Bootstrapper/FirebaseLogInService.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FirebaseLogInService : MonoBehaviour
+{
+    private const string WebClientId = "1060578439326-0mbhma8b723jga0d8ndhsk2qv50go5tt.apps.googleusercontent.com";
+
+    [Header("에디터 로그인 (이메일 로그인)")]
+    [SerializeField] private string _email;
+    [SerializeField] private string _password;
+
+    private const LogCategory CurrentCategory = LogCategory.Firebase;
+
+  
+}
