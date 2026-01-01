94e36a3f895f6b6add00026309885ae2a390796e
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-02 02:28:25 +0900

    feat: 구글 및 이메일 로그인 기능 추가

--- Assets/Scripts/Bootstrapper/Bootstrapper.cs
+++ Assets/Scripts/Bootstrapper/Bootstrapper.cs
@@ -7,7 +7,13 @@ public class Bootstrapper : MonoBehaviour
 {
     private const LogCategory CurrentCategory = LogCategory.Bootstrap;
 
+    [Header("모듈")]
+    [SerializeField] private FirebaseLogInService _firebaseLogInService;
+
+    [Header("재시도 정책")]
     [SerializeField] private int _maxRetry = 3;
+    [SerializeField] private int _firebaseInitStepTimeOutMs = 5000;
+    [SerializeField] private int _firebaseLogInStepTimeOutMs = 10000;
     [SerializeField] private int _retryDelayMs = 1000;
 
 
@@ -20,14 +26,17 @@ public class Bootstrapper : MonoBehaviour
 
     private async UniTask BootstrapFlowAsync(CancellationToken ct)
     {
-        await RunStepWithRetry("Firebase 초기화", () => FirebaseManager.Instance.Initialize(ct), ct);
-        // await 로그인 기능
+        await RunStepWithRetry("Firebase 초기화", () => FirebaseManager.Instance.Initialize(ct), _firebaseInitStepTimeOutMs, ct);
+        this.PrintLog("Firebase 초기화 완료", CurrentCategory, LogType.Log);
+
+        var user = await RunStepWithRetry("Firebase 로그인", () => _firebaseLogInService.SignInAsync(ct), _firebaseLogInStepTimeOutMs, ct);
+        this.PrintLog($"Firebase 로그인 완료: {user.UserId}", CurrentCategory, LogType.Log);
         // await 프로필 로드
         // await Pun2
     }
 
 
-    private async UniTask RunStepWithRetry(string stepName, Func<UniTask> step, CancellationToken ct)
+    private async UniTask RunStepWithRetry(string stepName, Func<UniTask> step, int stepTimeoutMs, CancellationToken ct)
     {
         for (int attempt = 1; attempt <= _maxRetry; attempt++)
         {
@@ -35,8 +44,34 @@ public class Bootstrapper : MonoBehaviour
             {
                 this.PrintLog($"{stepName} 시도, {attempt} / {_maxRetry}", CurrentCategory, LogType.Log);
 
-                await step().Timeout(TimeSpan.FromMilliseconds(_retryDelayMs));
+                await step().Timeout(TimeSpan.FromMilliseconds(stepTimeoutMs));
                 return;
+            }
+            catch (TimeoutException) when (attempt < _maxRetry)
+            {
+                this.PrintLog($"{stepName} 타임아웃, {attempt}/{_maxRetry}", CurrentCategory, LogType.Warning);
+            }
+            catch (Exception) when (attempt < _maxRetry)
+            {
+                this.PrintLog($"{stepName} 실패. {attempt}/{_maxRetry}", CurrentCategory, LogType.Warning);
+            }
+
+            await UniTask.Delay(_retryDelayMs, cancellationToken: ct);
+        }
+
+        // 최종 실패
+        throw new Exception($"{stepName} 실패");
+    }
+
+    private async UniTask<T> RunStepWithRetry<T>(string stepName, Func<UniTask<T>> step, int stepTimeoutMs, CancellationToken ct)
+    {
+        for (int attempt = 1; attempt <= _maxRetry; attempt++)
+        {
+            try
+            {
+                this.PrintLog($"{stepName} 시도, {attempt} / {_maxRetry}", CurrentCategory, LogType.Log);
+
+                return await step().Timeout(TimeSpan.FromMilliseconds(stepTimeoutMs));
             }
             catch (TimeoutException) when (attempt < _maxRetry)
             {
--- Assets/Scripts/Bootstrapper/FirebaseLogInService.cs
+++ Assets/Scripts/Bootstrapper/FirebaseLogInService.cs
@@ -1,14 +1,107 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using Firebase;
+using Firebase.Auth;
+using Google;
+using System;
+using System.Threading;
+using UnityEngine;
 
 public class FirebaseLogInService : MonoBehaviour
 {
-    private const string WebClientId = "1060578439326-0mbhma8b723jga0d8ndhsk2qv50go5tt.apps.googleusercontent.com";
-
-    [Header("에디터 로그인 (이메일 로그인)")]
+    [Header("에디터 로그인: 이메일 로그인")]
     [SerializeField] private string _email;
     [SerializeField] private string _password;
 
+    private const string WebClientId = "1060578439326-0mbhma8b723jga0d8ndhsk2qv50go5tt.apps.googleusercontent.com";
+
     private const LogCategory CurrentCategory = LogCategory.Firebase;
 
-  
+
+    private void Awake()
+    {
+        MakeConfiguration();
+    }
+
+
+    public async UniTask<FirebaseUser> SignInAsync(CancellationToken ct)
+    {
+#if UNITY_EDITOR
+        return await SignInOrCreateWithEmailAsync(_email, _password, ct);
+#endif
+        return await SignInWithGoogleAsync(ct);
+    }
+
+
+
+    private async UniTask<FirebaseUser> SignInOrCreateWithEmailAsync(
+        string email, string password, CancellationToken ct)
+    {
+
+#if !UNITY_EDITOR
+    throw new InvalidOperationException("이메일 로그인은 에디터에서만 사용합니다.");
+#else
+        if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+        {
+            throw new ArgumentException("email/password가 비어있음");
+        }
+
+        try
+        {
+            this.PrintLog("이메일 로그인 시도", CurrentCategory);
+
+            AuthResult signInResult = await FirebaseManager.Instance.Auth.SignInWithEmailAndPasswordAsync(email, password)
+                            .AsUniTask()
+                            .AttachExternalCancellation(ct);
+
+            return signInResult.User;
+        }
+        catch (FirebaseException e)
+        {
+            this.PrintLog($"이메일 로그인 중 FirebaseException 진입, {e}", CurrentCategory, LogType.Log);
+            AuthError error = (AuthError)e.ErrorCode;
+
+            this.PrintLog("이메일 로그인 회원가입 진입", CurrentCategory);
+
+            AuthResult created = await FirebaseManager.Instance.Auth.CreateUserWithEmailAndPasswordAsync(email, password)
+                .AsUniTask()
+                .AttachExternalCancellation(ct);
+
+            return created.User;
+        }
+#endif
+    }
+
+    private async UniTask<FirebaseUser> SignInWithGoogleAsync(CancellationToken ct)
+    {
+        // 1. 구글 로그인 UI
+        GoogleSignInUser gUser = await GoogleSignIn.DefaultInstance
+            .SignIn()
+            .AsUniTask()
+            .AttachExternalCancellation(ct);
+
+        if (string.IsNullOrEmpty(gUser.IdToken))
+            throw new Exception("GoogleSignInUser.IdToken이 비어있음 (WebClientId/RequestIdToken 설정 확인)");
+
+        // 2. Firebase에서 Credential 받기
+        Credential credential = GoogleAuthProvider.GetCredential(gUser.IdToken, null);
+
+        // 3. Firebase 로그인
+        FirebaseUser user = await FirebaseManager.Instance.Auth.SignInWithCredentialAsync(credential)
+            .AsUniTask()
+            .AttachExternalCancellation(ct);
+
+        return user;
+    }
+
+
+    private void MakeConfiguration()
+    {
+        GoogleSignIn.Configuration = new()
+        {
+            WebClientId = WebClientId,
+            RequestIdToken = true,
+            RequestEmail = false,
+            UseGameSignIn = false
+        };
+    }
 }
