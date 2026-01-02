debe03929439d53dbdee53a45a7f22ea43b652d2
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-02 19:18:30 +0900

    feat: UI 매니저 및 UI 애니메이션 시스템 추가

---
+++ Assets/Scripts/UI/UI System/SceneUIUnitRegistrar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SceneUIUnitRegistrar : MonoBehaviour
+{
+    private void Awake()
+    {
+        foreach (var unit in GetComponentsInChildren<UIUnit>(true))
+        {
+            string key = unit.name;
+            UIUnit go = unit;
+
+            UIManager.Instance.RegisterUnit(key, go);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // 만약 씬이 바뀌어도 유지되어야 하는 유닛이 있다면 이거 수정하고 직접 key 넣어서 지우도록 바꿔야 됨
+        UIManager.Instance.ClearUnit();
+    }
+}
