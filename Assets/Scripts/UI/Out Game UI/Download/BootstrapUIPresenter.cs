03a647b107041aba5f785197884ab0f136ede7f3
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-02 14:48:51 +0900

    feat: 어드레서블 CDN에서 다운로드 UI 로직 추가

---
+++ Assets/Scripts/UI/Out Game UI/Download/BootstrapUIPresenter.cs
@@ -0,0 +1,31 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BootstrapUIPresenter : MonoBehaviour
+{
+    [SerializeField] private Button _startDownloadBtn;
+
+    private UniTaskCompletionSource _downloadTcs;
+
+    public void Init()
+    {
+        _startDownloadBtn.gameObject.SetActive(false);
+        _startDownloadBtn.onClick.RemoveAllListeners();
+        _startDownloadBtn.onClick.AddListener(OnClickStartDownload);
+    }
+
+    public UniTask WaitForDownloadClickAsync()
+    {
+        _downloadTcs = new UniTaskCompletionSource();
+        _startDownloadBtn.gameObject.SetActive(true);
+        return _downloadTcs.Task;
+    }
+
+    private void OnClickStartDownload()
+    {
+        _startDownloadBtn.gameObject.SetActive(false);
+        _downloadTcs?.TrySetResult();
+    }
+}
