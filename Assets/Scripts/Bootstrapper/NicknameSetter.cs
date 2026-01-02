a311c68ec3695f764f726f7abc27ed50fc9dc403
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-02 18:24:55 +0900

    feat: 닉네임 설정 기능 추가

---
+++ Assets/Scripts/Bootstrapper/NicknameSetter.cs
@@ -0,0 +1,61 @@
+﻿using Cysharp.Threading.Tasks;
+using Firebase.Database;
+using System.Threading;
+using UnityEngine;
+
+public enum NicknameSetErrorCode
+{
+    None,
+    Success,
+    AlreadyExist,
+    NotAllowed,     // TODO: 비속어 등
+    Empty,
+    ElseError
+}
+
+
+public class NicknameSetter : MonoBehaviour
+{
+    private LogCategory _currentCategory = LogCategory.Firebase;
+
+    public async UniTask<bool> IsSetNickname(string uid, CancellationToken ct)
+    {
+        var nicknameSnap = await RTDBService.Instance.GetUniTaskAsync(DBRoutes.Nickname(uid), ct);
+
+        // 닉네임 없으면 최초 회원가입 혹은 uid는 있는데 닉네임 설정 안한 것
+        if (!nicknameSnap.Exists) return false;
+        else return true;
+    }
+
+
+    public async UniTask<NicknameSetErrorCode> TrySetNickname(string nickname, CancellationToken ct)
+    {
+        if (string.IsNullOrEmpty(nickname))
+        {
+            return NicknameSetErrorCode.Empty;
+        }
+
+        var result = await RTDBService.Instance.RunTransactionUniTaskAsync(
+            DBRoutes.NicknameStorage(nickname),
+            mutable =>
+            {
+                if (mutable.Value != null)
+                {
+                    return TransactionResult.Abort();
+                }
+
+                mutable.Value = nickname;
+                return TransactionResult.Success(mutable);
+            },
+            ct: ct
+        );
+
+        if (!result.Exists)
+        {
+            this.PrintLog("중복된 닉네임이 존재합니다.", _currentCategory, LogType.Log);
+            return NicknameSetErrorCode.AlreadyExist;
+        }
+
+        return NicknameSetErrorCode.Success;
+    }
+}
---
+++ Assets/Scripts/UI/Out Game UI/Nickname/NicknameSetUpView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NicknameSetUpView : MonoBehaviour
+{
+    [Header("닉네임 입력 UI")]
+    [SerializeField] private TMP_InputField _inputField;
+    [SerializeField] private Button _submitBtn;
+
+    [Header("에러 UI")]
+    [SerializeField] private GameObject _errorObj;
+    [SerializeField] private TMP_Text _errorText;
+
+    public event Action OnSubmitClicked;
+
+    private LogCategory _currentCategory = LogCategory.UI;
+
+    public string NicknameText => _inputField.text;
+
+
+    private void Awake()
+    {
+        this.TryBindComponent(ref _inputField, _currentCategory);
+    }
+
+    private void OnEnable()
+    {
+        _errorObj.SetActive(false);
+        _inputField.text = string.Empty;
+    }
+
+
+    public void ShowError(string error)
+    {
+        _errorObj.SetActive(true);
+        _errorText.text = error;
+    }
+
+    public void HideError()
+    {
+        _errorObj.SetActive(false);
+        _errorText.text = string.Empty;
+    }
+}
