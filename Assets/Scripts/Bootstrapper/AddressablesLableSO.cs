f3ef508409c259ab053b191be0840e10fadc6f7e
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-02 14:51:02 +0900

    feat: 어드레서블 라벨 관리용 SO 추가

---
+++ Assets/Scripts/Bootstrapper/AddressablesLableSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "ScriptableObjects/AddressablesLables")]
+public class AddressablesLableSO : ScriptableObject
+{
+    [SerializeField] private List<string> _labels = new();
+    public List<string> Labels => _labels;
+}
