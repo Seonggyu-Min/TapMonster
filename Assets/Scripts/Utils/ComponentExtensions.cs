c02457e1dd4b635f80b4ebc88848466371b093b2
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-02 18:25:10 +0900

    feat: 컴포넌트 찾기 및 등록 확장 메서드 추가

---
+++ Assets/Scripts/Utils/ComponentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class ComponentExtensions
+{
+    public static bool TryBindComponent<T>(
+        this Component owner,
+        ref T field,
+        LogCategory category,
+        LogType logType = LogType.Error)
+        where T : Component
+    {
+        if (field != null) return true;
+        if (owner.TryGetComponent(out field)) return true;
+
+        owner.PrintLog($"{typeof(T).Name} 컴포넌트를 찾을 수 없습니다.", category, logType);
+        return false;
+    }
+}
