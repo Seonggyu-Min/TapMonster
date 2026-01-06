848b42fe75ed92c9d75a83ef22aefe14b32e8769
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-06 11:14:43 +0900

    feat: 진행 상태 저장용 Model 추가

---
+++ Assets/Scripts/Battle Logic/Model/GameStateModel.cs
@@ -0,0 +1,31 @@
+﻿public class GameStateModel
+{
+    private RelicModel _relicModel;
+    private SkillModel _skillModel;
+    private StageModel _stageModel;
+    private UpgradeModel _upgradeModel;
+    private WalletModel _walletModel;
+
+
+    public GameStateModel(
+        RelicModel relicModel,
+        SkillModel skillModel,
+        StageModel stageModel,
+        UpgradeModel upgradeModel,
+        WalletModel walletModel
+        )
+    {
+        _relicModel = relicModel;
+        _skillModel = skillModel;
+        _stageModel = stageModel;
+        _upgradeModel = upgradeModel;
+        _walletModel = walletModel;
+    }
+
+
+    public RelicModel RelicModel => _relicModel;
+    public SkillModel SkillModel => _skillModel;
+    public StageModel StageModel => _stageModel;
+    public UpgradeModel UpgradeModel => _upgradeModel;
+    public WalletModel WalletModel => _walletModel;
+}
