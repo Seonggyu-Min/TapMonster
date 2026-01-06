a5141c0275506270b6b006996dd8205e1c510785
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-06 11:16:26 +0900

    feat: 스킬 시스템 관리용 클래스 추가

---
+++ Assets/Scripts/Battle Logic/Skill/SkillManager.cs
@@ -0,0 +1,21 @@
+﻿public class SkillManager : IStatContributor, IStatModifier
+{
+    private SkillService _skillService;
+    private GameConfigSO _gameConfigSO;
+
+    public SkillManager(SkillService skillService, GameConfigSO gameConfigSO)
+    {
+        _skillService = skillService;
+        _gameConfigSO = gameConfigSO;
+    }
+
+    public void Contribute(ref PlayerStatBuildContext buildContext)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Modify(ref DamageContext damageContext)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Battle Logic/Skill/SkillService.cs
@@ -0,0 +1,9 @@
+﻿public class SkillService
+{
+    private SkillModel _skillModel;
+
+    public SkillService(SkillModel skillModel)
+    {
+        _skillModel = skillModel;
+    }
+}
