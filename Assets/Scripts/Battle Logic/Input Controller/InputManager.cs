4852001f0f85c3060af5b43fd21d8be68092c57d
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-06 11:13:48 +0900

    feat: 각 공격 이벤트 발행용 InputManager 추가

---
+++ Assets/Scripts/Battle Logic/Input Controller/InputManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private Button _manualAttackButton;
+    // TODO: 스킬 버튼 List
+
+    public event Action OnManualAttack;
+
+
+    private void OnEnable()
+    {
+        HandleSubscribe(true);
+    }
+
+    private void OnDisable()
+    {
+        HandleSubscribe(false);
+    }
+
+
+    private void HandleSubscribe(bool willSubscribe)
+    {
+        if (willSubscribe)
+        {
+            _manualAttackButton.onClick.AddListener(InvokeManualAttack);
+        }
+        else
+        {
+            _manualAttackButton.onClick.RemoveListener(InvokeManualAttack);
+        }
+    }
+
+
+    private void InvokeManualAttack()
+    {
+        this.PrintLog("이벤트 발생");
+        OnManualAttack?.Invoke();
+    }
+}
