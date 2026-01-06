397f3a45a96ce7d959b1fbf7a304bb6627bbc411
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-06 11:16:13 +0900

    feat: 상점 로직 관리용 클래스 추가

---
+++ Assets/Scripts/Battle Logic/Shop/ShopManager.cs
@@ -0,0 +1,9 @@
+﻿public class ShopManager
+{
+    private ShopService _shopService;
+
+    public ShopManager(ShopService shopService)
+    {
+        _shopService = shopService;
+    }
+}
---
+++ Assets/Scripts/Battle Logic/Shop/ShopService.cs
@@ -0,0 +1,12 @@
+﻿public class ShopService
+{
+    private RelicModel _relicModel;
+    private WalletModel _walletModel;
+
+
+    public ShopService(RelicModel relicModel, WalletModel walletModel)
+    {
+        _relicModel = relicModel;
+        _walletModel = walletModel;
+    }
+}
