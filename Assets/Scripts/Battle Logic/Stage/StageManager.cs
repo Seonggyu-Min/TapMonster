df6e9272db9279a6779f4b781faaba12f93d3eba
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-06 11:16:34 +0900

    feat: 스테이지 시스템 관리용 클래스 추가

---
+++ Assets/Scripts/Battle Logic/Stage/StageManager.cs
@@ -0,0 +1,12 @@
+﻿public class StageManager
+{
+    private StageService _stageService;
+    private GameConfigSO _gameConfigSO;
+
+
+    public StageManager(StageService stageService, GameConfigSO gameConfigSO)
+    {
+        _stageService = stageService;
+        _gameConfigSO = gameConfigSO;
+    }
+}
---
+++ Assets/Scripts/Battle Logic/Stage/StageService.cs
@@ -0,0 +1,10 @@
+﻿public class StageService
+{
+    private StageModel _stageModel;
+
+
+    public StageService(StageModel stageModel)
+    {
+        _stageModel = stageModel;
+    }
+}
