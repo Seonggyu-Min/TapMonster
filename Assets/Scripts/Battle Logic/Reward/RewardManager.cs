4113e9cc97fcc88ca7044c31fb586caedd5d53ce
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-06 11:15:49 +0900

    feat: 보상체계 관리용 클래스 추가

---
+++ Assets/Scripts/Battle Logic/Reward/RewardManager.cs
@@ -0,0 +1,11 @@
+﻿public class RewardManager
+{
+    private RewardService _rewardService;
+    private GameConfigSO _gameConfigSO;
+
+    public RewardManager(RewardService rewardService, GameConfigSO gameConfigSO)
+    {
+        _rewardService = rewardService;
+        _gameConfigSO = gameConfigSO;
+    }
+}
