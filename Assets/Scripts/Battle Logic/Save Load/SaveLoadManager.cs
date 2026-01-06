6aa7dec2951c621e79555377f5c698a99c2bb6ab
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-06 11:16:02 +0900

    feat: 세이브 및 로드 관리용 클래스 추가

---
+++ Assets/Scripts/Battle Logic/Save Load/SaveLoadManager.cs
@@ -0,0 +1,45 @@
+﻿public class SaveLoadManager
+{
+    private SaveLoadService _saveLoadService;
+
+    private StageManager _stageManager;
+    private RelicManager _relicManager;
+    private UpgradeManager _upgradeManager;
+    private SkillManager _skillManager;
+    private WalletManager _walletManager;
+
+    public SaveLoadManager(
+        SaveLoadService saveLoadService,
+        StageManager stageManager,
+        RelicManager relicManager,
+        UpgradeManager upgradeManager,
+        SkillManager skillManager,
+        WalletManager walletManager
+        )
+    {
+        _saveLoadService = saveLoadService;
+
+        _stageManager = stageManager;
+        _relicManager = relicManager;
+        _upgradeManager = upgradeManager;
+        _skillManager = skillManager;
+        _walletManager = walletManager;
+    }
+
+
+    public void Save()
+    {
+
+    }
+
+    public void LoadAll()
+    {
+        _saveLoadService.LoadAll(
+            stageManager: _stageManager,
+            relicManager: _relicManager,
+            upgradeManager: _upgradeManager,
+            skillManager: _skillManager,
+            walletManager: _walletManager
+            );
+    }
+}
---
+++ Assets/Scripts/Battle Logic/Save Load/SaveLoadService.cs
@@ -0,0 +1,42 @@
+﻿public class SaveLoadService
+{
+    public void LoadAll(
+        StageManager stageManager,
+        RelicManager relicManager,
+        UpgradeManager upgradeManager,
+        SkillManager skillManager,
+        WalletManager walletManager
+        )
+    {
+
+
+        
+    }
+
+
+
+    private void InjectDataToStageManager()
+    {
+        //RTDBService.Instance.GetUniTaskAsync()
+    }
+
+    private void InjectDataToRelicManager()
+    {
+
+    }
+
+    private void InjectDataToUpgradeManager()
+    {
+
+    }
+
+    private void InjectDataToSkillManager()
+    {
+
+    }
+
+    private void InjectDataToWalletManager()
+    {
+
+    }
+}
