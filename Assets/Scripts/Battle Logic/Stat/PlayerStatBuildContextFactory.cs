48172521b2404430a83b7cada376f8d3ae0f528a
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:15:20 +0900

    feat: PlayerStatBuildContext 생성용 팩토리 추가

---
+++ Assets/Scripts/Battle Logic/Stat/PlayerStatBuildContextFactory.cs
@@ -0,0 +1,24 @@
+﻿public static class PlayerStatBuildContextFactory
+{
+    public static PlayerStatBuildContext CreateBase(float autoInterval = 0.2f)
+    {
+        return new PlayerStatBuildContext
+        {
+            ManualDamage = BigNumber.One,
+            AutoDamage = BigNumber.One,
+
+            AutoDamageInterval = autoInterval,
+
+            ManualCriticalChance = 0f,
+            AutoCriticalChance = 0f,
+
+            ManualAdditiveDamagePercent = 0f,
+            AutoAdditiveDamagePercent = 0f,
+
+            ManualDamageMultiplier = 1f,
+            AutoDamageMultiplier = 1f,
+
+            FinalAllDamageMultiplier = 1f,
+        };
+    }
+}
