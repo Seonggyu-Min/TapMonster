0a92273bf8e67a703d0beabd54b49eee2bd2e5fa
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:09:35 +0900

    feat: 재화 획득 및 사용 관련 기능 추가

--- Assets/Scripts/Battle Logic/Money/WalletManager.cs
+++ Assets/Scripts/Battle Logic/Money/WalletManager.cs
@@ -1,4 +1,31 @@
-﻿public class WalletManager
-{
+﻿using System;
 
+public class WalletManager
+{
+    private readonly ISaveMark _saveMark;
+    private WalletService _walletService;
+
+    public event Action<CurrencyId, BigNumber> OnCurrencyChanged;   // UI에서 구독 필요없으면 없애도 될 듯
+
+    public WalletManager(WalletService walletService, ISaveMark saveMark)
+    {
+        _walletService = walletService;
+        _saveMark = saveMark;
+    }
+
+    public BigNumber Get(CurrencyId id) => _walletService.Get(id);
+
+    public void Earn(CurrencyId id, BigNumber amount, bool requestSave = true)
+    {
+        _walletService.Earn(id, amount);
+        OnCurrencyChanged?.Invoke(id, _walletService.Get(id));
+
+        if (requestSave)
+        {
+            _saveMark.MarkDirty(SaveDirtyFlags.Wallet);
+            _saveMark.RequestSave();
+        }
+    }
+
+    public bool CanAfford(CurrencyId id, BigNumber cost) => _walletService.CanAfford(id, cost);
 }
--- Assets/Scripts/Battle Logic/Money/WalletService.cs
+++ Assets/Scripts/Battle Logic/Money/WalletService.cs
@@ -1,4 +1,11 @@
-﻿public class WalletService
+﻿public enum SpendResult
+{
+    Success,
+    NotEnough,
+    InvalidAmount
+}
+
+public class WalletService
 {
     private WalletModel _walletModel;
 
@@ -6,4 +13,26 @@
     {
         _walletModel = walletModel;
     }
+
+    public BigNumber Get(CurrencyId id) => _walletModel.Get(id);
+
+    public void Earn(CurrencyId id, BigNumber amount)
+    {
+        if (amount.Mantissa <= 0) return; // 0 이하의 수익은 무시
+        _walletModel.Set(id, _walletModel.Get(id) + amount);
+    }
+
+    public SpendResult TrySpend(CurrencyId id, BigNumber cost)
+    {
+        if (cost.Mantissa <= 0) return SpendResult.InvalidAmount;
+
+        BigNumber cur = _walletModel.Get(id);
+        if (cur < cost) return SpendResult.NotEnough;
+
+        _walletModel.Set(id, cur - cost);
+        return SpendResult.Success;
+    }
+
+    public bool CanAfford(CurrencyId id, BigNumber cost)
+        => cost.Mantissa > 0 && _walletModel.Get(id) >= cost;
 }
