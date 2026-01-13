73f33c92f83ade81692ac897c9133cb97d57c03a
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:00:52 +0900

    feat: SO에셋 직렬화 추가

--- Assets/Scripts/Battle Logic/Config SO/GameConfigSO.cs
+++ Assets/Scripts/Battle Logic/Config SO/GameConfigSO.cs
@@ -7,10 +7,15 @@ public class GameConfigSO : ScriptableObject
     [SerializeField] private RewardConfigSO _rewardConfigSO;
     [SerializeField] private CombatConfigSO _combatConfigSO;
     [SerializeField] private GachaConfigSO _gachaConfigSO;
-
+    [SerializeField] private RelicConfigSO _relicConfigSO;
+    [SerializeField] private UpgradeConfigSO _upgradeConfigSO;
+    [SerializeField] private SkillConfigSO _skillConfigSO;
 
     public StageConfigSO StageConfigSO => _stageConfigSO;
     public RewardConfigSO RewardConfigSO => _rewardConfigSO;
     public CombatConfigSO CombatConfigSO => _combatConfigSO;
     public GachaConfigSO GachaConfigSO => _gachaConfigSO;
+    public RelicConfigSO RelicConfigSO => _relicConfigSO;
+    public UpgradeConfigSO UpgradeConfigSO => _upgradeConfigSO;
+    public SkillConfigSO SkillConfigSO => _skillConfigSO;
 }
