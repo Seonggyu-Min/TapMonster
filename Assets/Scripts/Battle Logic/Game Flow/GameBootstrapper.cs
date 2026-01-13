2c291f82ef045a491d885e99ad674014e8530cc6
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:02:30 +0900

    refactor: 부트스트랩 및 코디네이터 클래스 분리
    - 필요한 의존성 객체 생성 전용 부트스트래퍼와 구독 및 연결 동작 전용 코디네이터 클래스 분리

---
+++ Assets/Scripts/Battle Logic/Game Flow/GameBootstrapper.cs
@@ -0,0 +1,175 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+
+public class GameBootstrapper : MonoBehaviour
+{
+    [SerializeField] private GameCoordinator _coordinator;
+    [SerializeField] private GameConfigSO _gameConfigSO;
+
+    private GameContext _ctx;
+
+    // Models
+    private StageModel _stageModel;
+    private RelicModel _relicModel;
+    private UpgradeModel _upgradeModel;
+    private SkillModel _skillModel;
+    private WalletModel _walletModel;
+    private GameStateModel _gameStateModel;
+
+    // Services
+    private SaveLoadService _saveLoadService;
+    private SavePatchBuilder _savePatchBuilder;
+
+    private StatBuilderService _statBuilderService;
+
+    private StageService _stageService;
+
+    private RelicService _relicService;
+    private RelicGachaService _relicGachaService;
+
+    private UpgradeService _upgradeService;
+    private SkillService _skillService;
+
+    private CombatService _combatService;
+    private WalletService _walletService;
+
+    private RewardService _rewardService;
+    private PurchaseService _purchaseService;
+
+    // Managers
+    private SaveLoadManager _saveLoadManager;
+
+    private StageManager _stageManager;
+    private RelicManager _relicManager;
+    private UpgradeManager _upgradeManager;
+    private SkillManager _skillManager;
+
+    private CombatManager _combatManager;
+    private WalletManager _walletManager;
+
+    private RewardManager _rewardManager;
+    private PurchaseManager _purchaseManager;
+
+    private StatManager _statManager;
+
+
+
+    private void Awake()
+    {
+        _ctx = BuildContext();
+        _coordinator.Init(_ctx);
+
+        StartGameAsync(this.GetCancellationTokenOnDestroy()).Forget();
+    }
+
+
+    private async UniTaskVoid StartGameAsync(CancellationToken ct)
+    {
+        await _ctx.SaveLoadManager.LoadAllAsync(ct);
+        //_ctx.StatManager.RebuildSnapshot();
+    }
+
+    private GameContext BuildContext()
+    {
+        ConstructModels();
+        ConstructServices();
+        ConstructManagers();
+
+        return new GameContext
+        {
+            GameStateModel = _gameStateModel,
+
+            SaveLoadManager = _saveLoadManager,
+
+            StatManager = _statManager,
+            StageManager = _stageManager,
+
+            RelicManager = _relicManager,
+            UpgradeManager = _upgradeManager,
+            SkillManager = _skillManager,
+
+            CombatManager = _combatManager,
+            WalletManager = _walletManager,
+
+            RewardManager = _rewardManager,
+            PurchaseManager = _purchaseManager
+        };
+    }
+
+    private void ConstructModels()
+    {
+        _stageModel = new();
+        _relicModel = new();
+        _upgradeModel = new();
+        _skillModel = new();
+        _walletModel = new();
+
+        _gameStateModel = new(
+            relicModel: _relicModel,
+            skillModel: _skillModel,
+            stageModel: _stageModel,
+            upgradeModel: _upgradeModel,
+            walletModel: _walletModel
+        );
+    }
+
+    private void ConstructServices()
+    {
+        _saveLoadService = new();
+        _savePatchBuilder = new();
+
+        _statBuilderService = new();
+
+        _stageService = new(_gameStateModel.StageModel);
+
+        _relicService = new(_gameStateModel.RelicModel);
+        _relicGachaService = new(_gameStateModel.RelicModel, _gameConfigSO);
+
+        _upgradeService = new(_gameStateModel.UpgradeModel);
+        _skillService = new(_gameStateModel.SkillModel);
+
+        _combatService = new();
+        _walletService = new(_gameStateModel.WalletModel);
+
+        _rewardService = new();
+
+        _purchaseService = new(_walletService);
+    }
+
+    private void ConstructManagers()
+    {
+        string uid = FirebaseManager.Instance.Auth.CurrentUser.UserId;
+
+        _saveLoadManager = new(
+            uid: uid,
+            gameState: _gameStateModel,
+            service: _saveLoadService,
+            savePatchBuilder: _savePatchBuilder,
+            saveDebounceSeconds: 2.0f,
+            maxSaveIntervalSeconds: 15.0f
+        );
+
+        ISaveMark saveMark = _saveLoadManager;
+
+        _purchaseManager = new(_purchaseService, saveMark);
+        _stageManager = new(_stageService, _gameConfigSO, saveMark);
+        _relicManager = new(_relicService, _relicGachaService, _gameConfigSO, _purchaseManager, saveMark);
+        _upgradeManager = new(_upgradeService, _gameConfigSO, _purchaseManager, saveMark);
+        _skillManager = new(_skillService, _gameConfigSO, _purchaseManager, saveMark);
+        _combatManager = new(_combatService);
+        _walletManager = new(_walletService, saveMark);
+        _rewardManager = new(_rewardService, _gameConfigSO);
+
+        _statManager = new(
+            _statBuilderService,
+            new IStatContributor[]
+            {
+                _upgradeManager,
+                _relicManager,
+                _skillManager
+            },
+            _gameConfigSO
+        );
+    }
+}
--- Assets/Scripts/Battle Logic/Game Flow/GameCoordinator.cs
+++ Assets/Scripts/Battle Logic/Game Flow/GameCoordinator.cs
@@ -1,155 +1,98 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using EditorAttributes;
+using System.Threading;
+using UnityEngine;
 
 public class GameCoordinator : MonoBehaviour
 {
-    #region Fields and Properties
-
     [SerializeField] private InputManager _inputManager;
-    [SerializeField] private GameConfigSO _gameConfigSO;
+
+    private GameContext _gameContext;
+    private bool _initialized;
+
+    private const LogCategory CurrentCategory = LogCategory.GameLogic;
 
 
-    // Stat
-    private StatManager _statManager;
-    private StatBuilderService _statBuilderService;
-
-    // Stage
-    private StageManager _stageManager;
-    private StageService _stageService;
-    private StageModel _stageModel;
-
-    // Relic
-    private RelicManager _relicManager;
-    private RelicService _relicService;
-    private RelicGachaService _relicGachaService;
-    private RelicModel _relicModel;
-
-    // Upgrade
-    private UpgradeManager _upgradeManager;
-    private UpgradeService _upgradeService;
-    private UpgradeModel _upgradeModel;
-
-    // Skill
-    private SkillManager _skillManager;
-    private SkillService _skillService;
-    private SkillModel _skillModel;
-
-    // Combat
-    private CombatManager _combatManager;
-    private CombatService _combatService;
-
-    // Wallet
-    private WalletManager _walletManager;
-    private WalletService _walletService;
-    private WalletModel _walletModel;
-
-    // Reward
-    private RewardManager _rewardManager;
-    private RewardService _rewardService;
-
-    // Shop
-    private ShopManager _shopManager;
-    private ShopService _shopService;
-
-    // Model Set
-    private GameStateModel _gameStateModel;
-
-    #endregion
-
-
-    #region Unity Methods
-
-    private void Awake()
+    public void Init(GameContext ctx)
     {
-        ConstructModel();
-        ConstructAndInjectServices();
-        ConstructAndInjectManagers();
-    }
-
-    private void OnEnable()
-    {
-        HandleSubscribe(true);
-    }
-
-    private void OnDisable()
-    {
-        HandleSubscribe(false);
-    }
-
-    #endregion
-
-
-    #region Private Methods
-
-    private void ConstructModel()
-    {
-        _stageModel = new();
-        _relicModel = new();
-        _upgradeModel = new();
-        _skillModel = new();
-        _walletModel = new();
-
-        _gameStateModel = new(
-            relicModel: _relicModel,
-            skillModel: _skillModel,
-            stageModel: _stageModel,
-            upgradeModel: _upgradeModel,
-            walletModel: _walletModel
-            );
-    }
-
-    private void ConstructAndInjectServices()
-    {
-        _statBuilderService = new();
-        _stageService = new(_gameStateModel.StageModel);
-        _relicService = new(_gameStateModel.RelicModel);
-        _relicGachaService = new(_gameStateModel.RelicModel);
-        _upgradeService = new(_gameStateModel.UpgradeModel);
-        _skillService = new(_gameStateModel.SkillModel);
-        _combatService = new();
-        _walletService = new(_gameStateModel.WalletModel);
-        _rewardService = new();
-        _shopService = new(_relicModel, _walletModel);
-    }
-
-    private void ConstructAndInjectManagers()
-    {
-        _stageManager = new(_stageService, _gameConfigSO);
-        _relicManager = new(_relicService, _relicGachaService, _gameConfigSO);
-        _upgradeManager = new(_upgradeService, _gameConfigSO);
-        _walletManager = new();
-        _skillManager = new(_skillService, _gameConfigSO);
-        _combatManager = new(_combatService);
-        _rewardManager = new(_rewardService, _gameConfigSO);
-        _shopManager = new(_shopService);
-
-        _statManager = new(
-            _statBuilderService,
-            new IStatContributor[]
-            {
-                _upgradeManager,
-                _relicManager,
-                _skillManager
-            },
-        _gameConfigSO);
+        _gameContext = ctx;
+        _initialized = true;
+        HandleSubscribe();
     }
 
 
-    private void HandleSubscribe(bool willSubscribe)
+    private void OnDestroy()
     {
-        if (willSubscribe)
-        {
-            _inputManager.OnManualAttack += HandleManualAttack;
-
-        }
-
-
+        HandleUnsubscribe();
     }
 
+    private void OnApplicationPause(bool pause)
+    {
+        if (!pause) return;
+        if (!_initialized) return;
+
+        ForceSaveOnExitAsync().Forget();
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (!_initialized) return;
+        ForceSaveOnExitAsync().Forget();
+    }
+
+
+    private void HandleSubscribe()
+    {
+        if (!_initialized) return;
+        if (_inputManager == null) return;
+
+        _inputManager.OnManualAttack += HandleManualAttack;
+    }
+
+    private void HandleUnsubscribe()
+    {
+        if (_inputManager == null) return;
+
+        _inputManager.OnManualAttack -= HandleManualAttack;
+    }
 
     private void HandleManualAttack()
     {
-        this.PrintLog("클릭");
+        this.PrintLog("탭 입력", CurrentCategory);
+        PlayerStatSnapshot snap = _gameContext.StatManager.GetOrBuildSnapshot();
+        this.PrintLog($"ManualDamage={snap.ManualFinalDamage.Mantissa}e{snap.ManualFinalDamage.Exponent}", CurrentCategory);
     }
 
-    #endregion
+#if UNITY_EDITOR
+    [Button("테스트용 업그레이드")]
+    public void TestUpgradeClicked()
+    {
+        bool ok = _gameContext.UpgradeManager.TryUpgrade(10001);
+        if (!ok) return;
+
+        // 스탯 재계산
+        _gameContext.StatManager.MarkDirty();
+
+        // 자동 저장
+        _gameContext.SaveLoadManager.MarkDirty(SaveDirtyFlags.Upgrade | SaveDirtyFlags.Wallet);
+        _gameContext.SaveLoadManager.RequestSave();
+    }
+
+
+    [Button("테스트용 돈 얻기")]
+    public void TestEarnGold()
+    {
+        BigNumber before = _gameContext.WalletManager.Get(CurrencyId.Gold);
+        this.PrintLog($"얻기 전 - Mantissa: {before.Mantissa}, Exponent: {before.Exponent}", CurrentCategory);
+        _gameContext.WalletManager.Earn(CurrencyId.Gold, new BigNumber(1, 10));
+        BigNumber after = _gameContext.WalletManager.Get(CurrencyId.Gold);
+        this.PrintLog($"얻은 후 - Mantissa: {after.Mantissa}, Exponent: {after.Exponent}", CurrentCategory);
+    }
+#endif
+
+    private async UniTaskVoid ForceSaveOnExitAsync()
+    {
+        CancellationToken ct = this.GetCancellationTokenOnDestroy();
+        await _gameContext.SaveLoadManager.ForceSaveAsync(ct);
+    }
 }
