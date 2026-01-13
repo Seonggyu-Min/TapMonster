90e2194ea8182dfe77d6977b3c6e373e35518148
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 17:50:05 +0900

    feat: 유물 가챠 관련 수치 SO 로직 추가

--- Assets/Scripts/Battle Logic/Config SO/GachaConfigSO.cs
+++ Assets/Scripts/Battle Logic/Config SO/GachaConfigSO.cs
@@ -1,7 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 [CreateAssetMenu(menuName = "ScriptableObjects/GachaConfig")]
 public class GachaConfigSO : ScriptableObject
 {
+    [Header("Cost")]
+    [SerializeField] private BigNumber _baseGachaCostGold = new BigNumber(1, 3); // 1e3
+    [SerializeField] private float _relicGachaCostGrowth = 1.15f;
 
+    [Header("Relic Pool")]
+    [SerializeField] private List<WeightedId> _relicPool = new();
+
+    [Serializable]
+    public struct WeightedId
+    {
+        public int Id;
+        public string Name;
+        public int Weight;
+        public Sprite Icon;
+    }
+
+    public BigNumber BaseRelicGachaCostGold => _baseGachaCostGold;
+    public float RelicGachaCostGrowth => _relicGachaCostGrowth;
+    public IReadOnlyList<WeightedId> RelicPool => _relicPool;
 }
