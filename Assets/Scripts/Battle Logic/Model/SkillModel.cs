f8ffd245fdb9554ad6cddf5324387d5ba121bb21
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:08:44 +0900

    feat: 스킬 모델 기능 추가

--- Assets/Scripts/Battle Logic/Model/SkillModel.cs
+++ Assets/Scripts/Battle Logic/Model/SkillModel.cs
@@ -1,4 +1,14 @@
-﻿public class SkillModel
-{
+﻿using UnityEngine;
+using System.Collections.Generic;
 
+public class SkillModel : ILevelModel
+{
+    private readonly Dictionary<int, int> _skillLevels = new();
+    public IReadOnlyDictionary<int, int> SkillLevels => _skillLevels;
+
+
+    public int GetLevel(int upgradeId) => _skillLevels.TryGetValue(upgradeId, out int level) ? level : 0;
+    public void SetLevel(int upgradeId, int level) => _skillLevels[upgradeId] = Mathf.Max(0, level);
+    public void AddLevel(int upgradeId, int delta) => SetLevel(upgradeId, GetLevel(upgradeId) + delta);
+    public void Clear() => _skillLevels.Clear();
 }
