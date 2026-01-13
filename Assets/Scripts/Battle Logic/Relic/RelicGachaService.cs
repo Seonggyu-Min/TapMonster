43e78e31375bba7cff42a03479e19137fac3a7d9
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:10:38 +0900

    feat: 유물 가챠 및 가격 조회 기능 추가

--- Assets/Scripts/Battle Logic/Relic/RelicGachaService.cs
+++ Assets/Scripts/Battle Logic/Relic/RelicGachaService.cs
@@ -1,9 +1,47 @@
-﻿public class RelicGachaService
-{
-    private RelicModel _relicModel;
+﻿using System;
+using Unity.VisualScripting.FullSerializer;
 
-    public RelicGachaService(RelicModel relicModel)
+public class RelicGachaService
+{
+    private GameConfigSO _gameConfigSO;
+    private RelicModel _relicModel;
+    private readonly Random _rng = new Random();
+
+    public RelicGachaService(RelicModel relicModel, GameConfigSO gameConfigSO)
     {
         _relicModel = relicModel;
+        _gameConfigSO = gameConfigSO;
+    }
+
+    public BigNumber GetCurrentRelicGachaCost()
+    {
+        int count = _relicModel.TotalRelicGachaCount;
+
+        double mul = Math.Pow(_gameConfigSO.GachaConfigSO.RelicGachaCostGrowth, count);
+        return _gameConfigSO.GachaConfigSO.BaseRelicGachaCostGold * mul;
+    }
+
+
+    public int RollRelicId(GameConfigSO config)
+    {
+        var pool = config.GachaConfigSO.RelicPool;
+        if (pool == null || pool.Count == 0) return 0;
+
+        int total = 0;
+        for (int i = 0; i < pool.Count; i++)
+            total += Math.Max(0, pool[i].Weight);
+
+        if (total <= 0) return pool[0].Id;
+
+        int r = _rng.Next(0, total);
+        int acc = 0;
+
+        for (int i = 0; i < pool.Count; i++)
+        {
+            acc += Math.Max(0, pool[i].Weight);
+            if (r < acc) return pool[i].Id;
+        }
+
+        return pool[pool.Count - 1].Id;
     }
 }
--- Assets/Scripts/Battle Logic/Relic/RelicManager.cs
+++ Assets/Scripts/Battle Logic/Relic/RelicManager.cs
@@ -3,17 +3,47 @@
     private RelicService _relicService;
     private RelicGachaService _relicGachaService;
     private GameConfigSO _gameConfigSO;
+    private PurchaseManager _purchaseManager;
+    private ISaveMark _saveMark;
 
-    public RelicManager(RelicService relicService, RelicGachaService relicGachaService, GameConfigSO gameConfigSO)
+    public RelicManager(
+        RelicService relicService,
+        RelicGachaService relicGachaService,
+        GameConfigSO gameConfigSO,
+        PurchaseManager purchaseManager,
+        ISaveMark saveMark
+        )
     {
         _relicService = relicService;
         _relicGachaService = relicGachaService;
         _gameConfigSO = gameConfigSO;
+        _purchaseManager = purchaseManager;
+        _saveMark = saveMark;
     }
 
 
+
+    public void TryGachaOnce()
+    {
+        BigNumber cost = _relicGachaService.GetCurrentRelicGachaCost();
+
+        var result = _purchaseManager.TryPay(new Cost(CurrencyId.Gold, cost));
+        if (result != PurchaseResult.Success) return;
+
+        int relicId = _relicGachaService.RollRelicId(_gameConfigSO);
+
+        // 중복이면 레벨업
+        // TODO: 풀 레벨 업 이면 다른 처리
+        _relicService.AddLevel(relicId, +1);
+
+        _relicService.IncrementRelicGachaCount();
+
+        _saveMark.MarkDirty(SaveDirtyFlags.Relic);
+        _saveMark.RequestSave();
+    }
+
     public void Contribute(ref PlayerStatBuildContext buildContext)
     {
-        throw new System.NotImplementedException();
+        _relicService.ApplyToStat(ref buildContext, _gameConfigSO);
     }
 }
