25a06c5a4dd2fa5a447f596297e6e59f61a9cf2e
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:16:08 +0900

    feat: 스냅샷 빌드 기능 제공

--- Assets/Scripts/Battle Logic/Stat/StatManager.cs
+++ Assets/Scripts/Battle Logic/Stat/StatManager.cs
@@ -4,10 +4,35 @@
     private IStatContributor[] _statContributors;
     private GameConfigSO _gameConfigSO;
 
+    private bool _dirty = true;
+    private PlayerStatSnapshot _cached;
+
     public StatManager(StatBuilderService statBuilderService, IStatContributor[] statContributors, GameConfigSO gameConfigSO)
     {
         _statBuilderService = statBuilderService;
         _statContributors = statContributors;
         _gameConfigSO = gameConfigSO;
+    }
+
+    public void MarkDirty()
+    {
+        _dirty = true;
+    }
+
+    public PlayerStatSnapshot GetOrBuildSnapshot()
+    {
+        if (_dirty)
+        {
+            _cached = _statBuilderService.BuildSnapshot(_gameConfigSO, _statContributors);
+            _dirty = false;
+        }
+        return _cached;
+    }
+
+    public PlayerStatSnapshot ForceRebuildSnapshot()
+    {
+        _cached = _statBuilderService.BuildSnapshot(_gameConfigSO, _statContributors);
+        _dirty = false;
+        return _cached;
     }
 }
