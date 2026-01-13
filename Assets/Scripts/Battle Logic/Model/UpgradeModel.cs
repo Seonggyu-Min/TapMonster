60caf7293d5603c59c7681036408742513d61bf2
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:08:55 +0900

    feat: 업그레이드 모델 기능 추가

--- Assets/Scripts/Battle Logic/Model/UpgradeModel.cs
+++ Assets/Scripts/Battle Logic/Model/UpgradeModel.cs
@@ -1,4 +1,15 @@
-﻿public class UpgradeModel
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeModel : ILevelModel
 {
+    private readonly Dictionary<int, int> _upgradeLevels = new();
+    public IReadOnlyDictionary<int, int> UpgradeLevels => _upgradeLevels;
+
+
+    public int GetLevel(int upgradeId) => _upgradeLevels.TryGetValue(upgradeId, out int level) ? level : 1;
+    public void SetLevel(int upgradeId, int level) => _upgradeLevels[upgradeId] = Mathf.Max(1, level);
+    public void AddLevel(int upgradeId, int delta) => SetLevel(upgradeId, GetLevel(upgradeId) + delta);
+    public void Clear() => _upgradeLevels.Clear();
 
 }
