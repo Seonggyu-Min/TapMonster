869b8863956c6daa80d486b4ba63ecf47feab759
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:02:50 +0900

    feat: 의존성 전달용 래퍼 객체 추가

---
+++ Assets/Scripts/Battle Logic/Game Flow/GameContext.cs
@@ -0,0 +1,14 @@
+﻿public class GameContext
+{
+    public GameStateModel GameStateModel { get; init; }
+    public SaveLoadManager SaveLoadManager { get; init; }
+    public StageManager StageManager { get; init; }
+    public RelicManager RelicManager { get; init; }
+    public UpgradeManager UpgradeManager { get; init; }
+    public SkillManager SkillManager { get; init; }
+    public WalletManager WalletManager { get; init; }
+    public CombatManager CombatManager { get; init; }
+    public RewardManager RewardManager { get; init; }
+    public PurchaseManager PurchaseManager { get; init; }
+    public StatManager StatManager { get; init; }
+}
