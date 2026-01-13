975add7e561af3c36f26fd846777e33889c1f01b
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:10:06 +0900

    feat: 비용 계산용 구조체 추가
    - 추후 할인율 등 추가하여 관리

---
+++ Assets/Scripts/Battle Logic/Purchase/Cost.cs
@@ -0,0 +1,12 @@
+﻿public readonly struct Cost
+{
+    public readonly CurrencyId Currency;
+    public readonly BigNumber Amount;
+    // TODO: 할인율 추가
+
+    public Cost(CurrencyId currency, BigNumber amount)
+    {
+        Currency = currency;
+        Amount = amount;
+    }
+}
