a393bde79ac16adaab30719780de47df819a8f64
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:15:05 +0900

    refactor: 멤버 변수명 변경

--- Assets/Scripts/Battle Logic/Stat/PlayerStatBuildContext.cs
+++ Assets/Scripts/Battle Logic/Stat/PlayerStatBuildContext.cs
@@ -8,12 +8,14 @@ public struct PlayerStatBuildContext
 
     public float AutoDamageInterval;                            // 자동 공격 데미지 주기 (일단 고정으로 쓸 것 같음)
 
+    public float ManualAdditiveDamagePercent;                   // 수동 공격 합연산
+    public float AutoAdditiveDamagePercent;                     // 자동 공격 합연산
+
+    public float ManualDamageMultiplier;                        // 수동 공격 곱연산
+    public float AutoDamageMultiplier;                          // 자동 공격 곱연산
+
+    public float FinalAllDamageMultiplier;                      // 전체 공격력 곱연산
+ 
     public float ManualCriticalChance;                          // 수동 공격 치명타 확률 (합연산만 가능)
     public float AutoCriticalChance;                            // 자동 공격 치명타 확률 (합연산만 가능)
-
-    public float ManualAdditiveDamageMultiplier;                // 수동 공격 합연산
-    public float AutoAdditiveDamageMultiplier;                  // 자동 공격 합연산
-
-    public float ManualMultiplicativeDamageMultiplier;          // 수동 공격 곱연산
-    public float AutoMultiplicativeDamageMultiplier;            // 자동 공격 곱연산
 }
