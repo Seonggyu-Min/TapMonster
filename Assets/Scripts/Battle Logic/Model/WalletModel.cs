d26e65b8243ec408227d7801a214df6a26313353
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:09:05 +0900

    feat: 지갑 모델 기능 추가

--- Assets/Scripts/Battle Logic/Model/WalletModel.cs
+++ Assets/Scripts/Battle Logic/Model/WalletModel.cs
@@ -1,4 +1,18 @@
-﻿public class WalletModel
-{
+﻿using System.Collections.Generic;
 
+public enum CurrencyId { Gold = 1, Gem = 2 }
+
+public class WalletModel
+{
+    private readonly Dictionary<CurrencyId, BigNumber> _values = new();
+    public IReadOnlyDictionary<CurrencyId, BigNumber> Values => _values;
+
+    public BigNumber Get(CurrencyId id)
+        => _values.TryGetValue(id, out BigNumber v) ? v : BigNumber.Zero;
+
+    public void Set(CurrencyId id, BigNumber value)
+        => _values[id] = value;
+
+    public void Add(CurrencyId id, BigNumber delta)
+        => Set(id, Get(id) + delta);
 }
