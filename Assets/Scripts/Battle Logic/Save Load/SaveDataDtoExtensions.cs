53539fd47fa995ed4c3db3c3f85130a7a74c9faa
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:12:22 +0900

    feat: null 대응 확장 메서드 추가

---
+++ Assets/Scripts/Battle Logic/Save Load/SaveDataDtoExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+public static class SaveDataDtoExtensions
+{
+    public static SaveDataDTO Normalized(this SaveDataDTO dto)
+    {
+        dto ??= new SaveDataDTO();
+
+        dto.StageDTO ??= new StageDTO();
+        dto.WalletDTO ??= new WalletDTO();
+
+        dto.WalletDTO.Currencies ??= new Dictionary<string, BigNumberDTO>();
+
+        dto.RelicLevels ??= new Dictionary<string, int>();
+        dto.UpgradeLevels ??= new Dictionary<string, int>();
+        dto.SkillLevels ??= new Dictionary<string, int>();
+
+        return dto;
+    }
+}
