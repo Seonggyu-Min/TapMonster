6bb4d68bfb8bfcc8d7d2d692d41de24e0f64951e
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:08:49 +0900

    feat: 스테이지 모델 기능 추가

--- Assets/Scripts/Battle Logic/Model/StageModel.cs
+++ Assets/Scripts/Battle Logic/Model/StageModel.cs
@@ -1,4 +1,13 @@
-﻿public class StageModel
-{
+﻿using UnityEngine;
 
+public class StageModel
+{
+    private int _currentStage = -1;
+
+    public int CurrentStage => _currentStage;
+
+    public void SetStage(int stage)
+    {
+        _currentStage = Mathf.Clamp(stage, 1, int.MaxValue);
+    }
 }
