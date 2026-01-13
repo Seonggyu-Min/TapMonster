7b58169dcf95a0849602bd6d80c14048fd39dee4
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:16:43 +0900

    feat: 업그레이드 기능 추가

--- Assets/Scripts/Battle Logic/Upgrade/UpgradeManager.cs
+++ Assets/Scripts/Battle Logic/Upgrade/UpgradeManager.cs
@@ -1,16 +1,46 @@
-﻿public class UpgradeManager : IStatContributor
+﻿using System;
+using Unity.VisualScripting.FullSerializer;
+
+public class UpgradeManager : IStatContributor
 {
     private UpgradeService _upgradeService;
     private GameConfigSO _gameConfigSO;
+    private PurchaseManager _purchaseManager;
+    private ISaveMark _saveMark;
 
-    public UpgradeManager(UpgradeService upgradeService, GameConfigSO gameConfigSO)
+    public UpgradeManager(
+        UpgradeService upgradeService,
+        GameConfigSO gameConfigSO,
+        PurchaseManager purchaseManager,
+        ISaveMark saveMark
+        )
     {
         _upgradeService = upgradeService;
         _gameConfigSO = gameConfigSO;
+        _purchaseManager = purchaseManager;
+        _saveMark = saveMark;
     }
+
+    public bool TryUpgrade(int upgradeId)
+    {
+        int nextLevel = _upgradeService.GetLevel(upgradeId) + 1;
+        BigNumber cost = _upgradeService.GetUpgradeCost(_gameConfigSO, upgradeId, nextLevel);
+
+        var result = _purchaseManager.TryPay(new Cost(CurrencyId.Gold, cost));
+        if (result != PurchaseResult.Success) return false;
+
+        _upgradeService.AddLevel(upgradeId, +1);
+
+        _saveMark.MarkDirty(SaveDirtyFlags.Upgrade);
+        _saveMark.RequestSave();
+
+        return true;
+    }
+
+    // TODO: 업그레이드 가격 조회
 
     public void Contribute(ref PlayerStatBuildContext buildContext)
     {
-        throw new System.NotImplementedException();
+        _upgradeService.ApplyToStat(ref buildContext, _gameConfigSO);
     }
 }
--- Assets/Scripts/Battle Logic/Upgrade/UpgradeService.cs
+++ Assets/Scripts/Battle Logic/Upgrade/UpgradeService.cs
@@ -1,9 +1,76 @@
-﻿public class UpgradeService
+﻿using System;
+
+public class UpgradeService
 {
     private UpgradeModel _upgradeModel;
 
     public UpgradeService(UpgradeModel upgradeModel)
     {
         _upgradeModel = upgradeModel;
+    }
+
+    public int GetLevel(int id) => _upgradeModel.GetLevel(id);
+    public void AddLevel(int id, int delta) => _upgradeModel.AddLevel(id, delta);
+
+    public BigNumber GetUpgradeCost(GameConfigSO config, int upgradeId, int nextLevel)
+    {
+        if (!config.UpgradeConfigSO.TryGet(upgradeId, out var def))
+            return BigNumber.One;
+
+        // base * growth^(nextLevel-1)
+        double mul = Math.Pow(def.CostGrowth, Math.Max(0, nextLevel - 1));
+        return def.BaseCost * mul;
+    }
+
+    public void ApplyToStat(ref PlayerStatBuildContext ctx, GameConfigSO config)
+    {
+        // 업그레이드 전체를 ctx에 누적
+        foreach (var kv in _upgradeModel.UpgradeLevels) // IReadOnlyDictionary<int,int> 필요
+        {
+            int id = kv.Key;
+            int level = kv.Value;
+            if (level <= 0) continue;
+
+            if (!config.UpgradeConfigSO.TryGet(id, out var def))
+                continue;
+
+            ApplyOne(ref ctx, def, level);
+        }
+    }
+
+    private static void ApplyOne(ref PlayerStatBuildContext ctx, UpgradeConfigSO.UpgradeDef def, int level)
+    {
+        double v = def.ValuePerLevel * level;
+
+        switch (def.StatType)
+        {
+            case UpgradeStatType.ManualAdditiveDamagePercent:
+                ctx.ManualAdditiveDamagePercent += (float)v;
+                break;
+
+            case UpgradeStatType.AutoAdditiveDamagePercent:
+                ctx.AutoAdditiveDamagePercent += (float)v;
+                break;
+
+            case UpgradeStatType.ManualDamageMultiplier:
+                ctx.ManualDamageMultiplier *= (float)v;
+                break;
+
+            case UpgradeStatType.AutoDamageMultiplier:
+                ctx.AutoDamageMultiplier *= (float)v;
+                break;
+
+            case UpgradeStatType.FinalAllDamageMultiplier:
+                ctx.FinalAllDamageMultiplier *= (float)v;
+                break;
+
+            case UpgradeStatType.ManualCriticalChance:
+                ctx.ManualCriticalChance += (float)v;
+                break;
+
+            case UpgradeStatType.AutoCriticalChance:
+                ctx.AutoCriticalChance += (float)v;
+                break;
+        }
     }
 }
