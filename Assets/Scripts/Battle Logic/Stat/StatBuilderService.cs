f6183856137a4e701341d29d8fa009a4154feb16
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:15:50 +0900

    feat: 최종 데미지 계산 기능 추가

--- Assets/Scripts/Battle Logic/Stat/StatBuilderService.cs
+++ Assets/Scripts/Battle Logic/Stat/StatBuilderService.cs
@@ -1,4 +1,30 @@
 ﻿public class StatBuilderService
 {
+    public PlayerStatSnapshot BuildSnapshot(
+        GameConfigSO config,
+        IStatContributor[] contributors
+        )
+    {
+        PlayerStatBuildContext ctx = PlayerStatBuildContextFactory.CreateBase();
 
+        // contributors가 ctx에 누적
+        if (contributors != null)
+        {
+            for (int i = 0; i < contributors.Length; i++)
+            {
+                contributors[i]?.Contribute(ref ctx);
+            }
+        }
+
+        // 최종 계산
+        BigNumber manualFinal = DamageFormula.ComputeManualFinal(ref ctx);
+        BigNumber autoFinal = DamageFormula.ComputeAutoFinal(ref ctx);
+
+        return new PlayerStatSnapshot(
+            manualFinal,
+            autoFinal,
+            ctx.ManualCriticalChance,
+            ctx.AutoCriticalChance,
+            ctx.AutoDamageInterval);
+    }
 }
