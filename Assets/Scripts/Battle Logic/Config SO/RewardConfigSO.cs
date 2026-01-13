09d7569ec2cc51d163f7e5fabbb1edaecf2fc10e
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 17:50:32 +0900

    feat: 보상 수치 관련 SO 로직 추가

--- Assets/Scripts/Battle Logic/Config SO/RewardConfigSO.cs
+++ Assets/Scripts/Battle Logic/Config SO/RewardConfigSO.cs
@@ -1,7 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [CreateAssetMenu(menuName = "ScriptableObjects/RewardConfig")]
 public class RewardConfigSO : ScriptableObject
 {
+    [Header("Gold Reward Curve")]
+    [SerializeField] private BigNumber _baseGold = new BigNumber(5, 1); // 50
+    [SerializeField] private double _goldGrowth = 1.12;
 
+    public BigNumber GetStageClearGold(int stage)
+    {
+        if (stage < 1) stage = 1;
+        int delta = stage - 1;
+        return _baseGold * Math.Pow(_goldGrowth, delta);
+    }
 }
