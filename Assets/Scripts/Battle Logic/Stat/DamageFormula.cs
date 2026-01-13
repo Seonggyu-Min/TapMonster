46ca735014cd7cd2942e44ea66dea9fa02d15ab8
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-13 18:14:52 +0900

    feat: 데미지 계산용 static 클래스 추가

---
+++ Assets/Scripts/Battle Logic/Stat/DamageFormula.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class DamageFormula
+{
+    public static BigNumber ComputeManualFinal(ref PlayerStatBuildContext ctx)
+    {
+        float add = Mathf.Max(0f, ctx.ManualAdditiveDamagePercent);
+        float mul = Mathf.Max(0f, ctx.ManualDamageMultiplier);
+        float all = Mathf.Max(0f, ctx.FinalAllDamageMultiplier);
+
+        // Base * (1 + add) * mul * all
+        BigNumber result = ctx.ManualDamage;
+        result *= (1.0 + add);
+        result *= mul;
+        result *= all;
+        return result;
+    }
+
+    public static BigNumber ComputeAutoFinal(ref PlayerStatBuildContext ctx)
+    {
+        float add = Mathf.Max(0f, ctx.AutoAdditiveDamagePercent);
+        float mul = Mathf.Max(0f, ctx.AutoDamageMultiplier);
+        float all = Mathf.Max(0f, ctx.FinalAllDamageMultiplier);
+
+        BigNumber result = ctx.AutoDamage;
+        result *= (1.0 + add);
+        result *= mul;
+        result *= all;
+        return result;
+    }
+}
