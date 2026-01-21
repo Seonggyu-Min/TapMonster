da516138a0235a5ccf60e306a4e850aaf62c085e
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:34:44 +0900

    feat: 하단 탭 패널 변환 기능 추가

---
+++ Assets/Scripts/UI/In Game UI/BottomTabSwitcher.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum TabType
+{
+    Upgrade,
+    Skill,
+    Relic,
+}
+
+[Serializable]
+public class TapEntry
+{
+    public TabType TabType;
+    public GameObject TabContent;
+}
+
+[Serializable]
+public class TapButtonEntry
+{
+    public TabType TabType;
+    public Button TabButton;
+}
+
+
+public class BottomTabSwitcher : MonoBehaviour
+{
+    [SerializeField] private List<TapEntry> _tabEntries;
+    [SerializeField] private List<TapButtonEntry> _tabButtonEntries;
+
+    private const LogCategory CurrentCategory = LogCategory.UI;
+
+
+    private void OnEnable()
+    {
+        foreach (TapButtonEntry buttonEntry in _tabButtonEntries)
+        {
+            Button button = buttonEntry.TabButton;
+            if (button == null) continue;
+
+            TabType tabType = buttonEntry.TabType;
+            button.onClick.AddListener(() => SwitchTab(tabType));
+        }
+
+        // 초기 탭 설정
+        if (_tabEntries.Count > 0)
+        {
+            SwitchTab(_tabEntries[0].TabType);
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (TapButtonEntry buttonEntry in _tabButtonEntries)
+        {
+            Button button = buttonEntry.TabButton;
+            if (button == null) continue;
+
+            button.onClick.RemoveAllListeners();
+        }
+    }
+
+    public void SwitchTab(TabType tabType)
+    {
+        foreach (var entry in _tabEntries)
+        {
+            bool isActive = entry.TabType == tabType;
+            entry.TabContent.SetActive(isActive);
+        }
+    }
+}
