4b82179849cbfabebe111fb95f2a34d4425162ec
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:36:59 +0900

    feat: 사이드 메뉴 버튼 관리용 기능 추가

---
+++ Assets/Scripts/UI/In Game UI/Side Menu/SideMenuBinder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SideMenuBinder : MonoBehaviour
+{
+    [SerializeField] private Button _skillInventoryButton;
+
+
+    private void OnEnable()
+    {
+        _skillInventoryButton.onClick.AddListener(OnClickSkillInventoryButton);
+    }
+
+    private void OnDisable()
+    {
+        _skillInventoryButton.onClick.RemoveListener(OnClickSkillInventoryButton);
+    }
+
+    private void OnClickSkillInventoryButton()
+    {
+        UIManager.Instance.Show(UIKey.SkillInventoryPanel);
+    }
+}
