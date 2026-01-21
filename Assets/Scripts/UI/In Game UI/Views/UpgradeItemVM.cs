1bb73071994c2b627b916ec13792713c01ae47eb
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:42:12 +0900

    feat: 업그레이드 아이템 모델 추가

---
+++ Assets/Scripts/UI/In Game UI/Views/UpgradeItemVM.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public readonly struct UpgradeItemVM : IBottomItemData
+{
+    public int Id { get; }
+    public string Name { get; }
+    public Sprite Icon { get; }
+
+    public int Level { get; }
+    public int MaxLevel { get; }
+
+    public Cost NextCost { get; }
+    public bool CanBuy { get; }
+
+    public bool IsMaxed => Level >= MaxLevel;
+
+    public UpgradeItemVM(
+        int id, string name, Sprite icon,
+        int level, int maxLevel,
+        Cost nextCost, bool canBuy)
+    {
+        Id = id;
+        Name = name;
+        Icon = icon;
+        Level = level;
+        MaxLevel = maxLevel;
+        NextCost = nextCost;
+        CanBuy = canBuy;
+    }
+}
