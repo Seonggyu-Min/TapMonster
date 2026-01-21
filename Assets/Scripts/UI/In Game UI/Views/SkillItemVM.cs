92ee068c8884fa6f4ea352080883daa15f8dac57
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:41:16 +0900

    feat: 스킬 아이템의 모델 기능 추가

---
+++ Assets/Scripts/UI/In Game UI/Views/SkillItemVM.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public struct SkillItemVM : IBottomItemData
+{
+    public int Id { get; }
+    public string Name { get; }
+    public Sprite Icon { get; }
+
+    public int Level { get; }
+    public int MaxLevel { get; }
+
+    public Cost NextCost { get; }
+    public bool CanBuy { get; }
+
+    public bool IsMaxed => Level >= MaxLevel;
+
+    public SkillItemVM(
+        int id, string name, Sprite icon,
+        int level, int maxLevel,
+        Cost nextCost, bool canBuy)
+    {
+        Id = id;
+        Name = name;
+        Icon = icon;
+        Level = level;
+        MaxLevel = maxLevel;
+        NextCost = nextCost;
+        CanBuy = canBuy;
+    }
+}
