8650776a3a80bc9b6e9a510af1a0c95742c22ebc
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 18:45:14 +0900

    feat: 스테이지 인덱스 관리용 서비스 추가

---
+++ Assets/Scripts/Battle Logic/Stage/StageProgressService.cs
@@ -0,0 +1,13 @@
+﻿public class StageProgressService
+{
+    private readonly StageModel _stageModel;
+
+    public StageProgressService(StageModel stageModel)
+    {
+        _stageModel = stageModel;
+    }
+
+    public int CurrentStage => _stageModel.CurrentStage;
+    public void SetStage(int stage) => _stageModel.SetStage(stage);
+    public void AdvanceStage() => _stageModel.SetStage(_stageModel.CurrentStage + 1);
+}
