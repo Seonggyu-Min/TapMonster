b286ffc03a537ae753875418146eff07cbf3d14d
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:41:05 +0900

    feat: 하단 패널의 스킬 UI 기능 추가

---
+++ Assets/Scripts/UI/In Game UI/Views/SkillItemView.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SkillItemView : BottomItemView<SkillItemVM>
+{
+    [SerializeField] private TMP_Text _levelText;
+    [SerializeField] private TMP_Text _costText;
+
+    protected override void ApplySpecific(SkillItemVM data)
+    {
+        _levelText.text = $"{data.Level}/{data.MaxLevel}";
+        _costText.text = data.IsMaxed ? "MAX" : data.NextCost.ToString();
+        _button.interactable = !data.IsMaxed && data.CanBuy;
+    }
+}
