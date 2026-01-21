4f36a421c9031242f14b1af4956026241a70c26b
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:44:17 +0900

    feat: Safe Area로 축소된 범위 복구 기능 추가

---
+++ Assets/Scripts/UI/SafeAreaBottomPaddingForLayoutGroup.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteAlways]
+public class SafeAreaBottomPaddingForLayoutGroup : MonoBehaviour
+{
+    [SerializeField] private VerticalLayoutGroup _layout;
+    [SerializeField] private int _extraPadding = 0; // 추가 여유
+
+    private int _lastBottom;
+
+    //private void OnEnable()
+    //{
+    //    StartCoroutine(Init());
+    //}
+
+    //private IEnumerator Init()
+    //{
+    //    yield return null;
+    //    Apply();
+    //}
+
+
+    private void OnRectTransformDimensionsChange()
+    {
+        Apply();
+    }
+
+    private void Apply()
+    {
+        Rect safe = Screen.safeArea;
+        int bottomInset = Mathf.RoundToInt(safe.y);
+
+        if (bottomInset == _lastBottom) return;
+        _lastBottom = bottomInset;
+
+        if (_layout == null) return;
+
+        RectOffset p = _layout.padding;
+        p.bottom = bottomInset + _extraPadding;
+        _layout.padding = p;
+
+        LayoutRebuilder.MarkLayoutForRebuild(_layout.transform as RectTransform);
+    }
+}
