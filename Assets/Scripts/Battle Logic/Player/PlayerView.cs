3ed5087dd4850d35974099e9f3a8de026d5471c1
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:27:34 +0900

    feat: 애니메이션 관리용 플레이어 뷰 추가

---
+++ Assets/Scripts/Battle Logic/Player/PlayerView.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    private const LogCategory CurrentCategory = LogCategory.Animation;
+
+
+    // 수동 공격 연출
+    public void PlayManualAttackAnimation(AnimationKey key)
+    {
+        Play(key);
+    }
+
+    // 스킬 연출
+    public void PlaySkillAnimation(AnimationKey key)
+    {
+        Play(key);
+    }
+
+    private void Play(AnimationKey key)
+    {
+        int hash = AnimationHashes.Get(AnimationSubject.Player, key);
+        if (hash == 0)
+        {
+            this.PrintLog($"애니메이션을 찾을 수 없습니다 key: {key}", CurrentCategory, LogType.Warning);
+            return;
+        }
+
+        _animator.SetTrigger(hash);
+    }
+}
