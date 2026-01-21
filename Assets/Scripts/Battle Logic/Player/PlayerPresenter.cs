baabe350d5b9abfd09552d1eeb38b3bc9fca8101
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:26:55 +0900

    feat: 플레이어 프레젠터 추가

---
+++ Assets/Scripts/Battle Logic/Player/PlayerPresenter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerPresenter : IDisposable
+{
+    private PlayerView _playerView;
+    private SkillManager _skillManager;
+    private InputManager _inputManager;
+    
+
+    public PlayerPresenter(
+        PlayerView playerView,
+        SkillManager skillManager,
+        InputManager inputManager
+        )
+    {
+        _playerView = playerView;
+        _skillManager = skillManager;
+        _inputManager = inputManager;
+    }
+
+
+    public void Initialize()
+    {
+        //_inputManager.OnManualAttack += HandleManualAttack;
+        //_skillManager.OnSkillUsed += HandleSkillUsed;
+
+    }
+
+
+
+    public void Dispose()
+    {
+        
+    }
+}
