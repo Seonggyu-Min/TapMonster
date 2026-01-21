b0e04926674ee5fe21bfb85dbf5b9981b0d1799a
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:35:50 +0900

    feat: 각 팝업 패널의 자신 닫기용 범용 기능 추가
    - UIKeyEnum을 통해 인스펙터 등록하여 자신을 닫는 기능을 하나의 코드로 사용할 수 있도록 기능 추가

---
+++ Assets/Scripts/UI/In Game UI/PanelCloser.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelCloser : MonoBehaviour
+{
+    [SerializeField] private UIKeyEnum _uIKeyEnumToClose;
+    [SerializeField] private Button _closeButton;
+
+
+    private void OnEnable()
+    {
+        if (_closeButton != null)
+        {
+            _closeButton.onClick.AddListener(ClosePanel);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_closeButton != null)
+        {
+            _closeButton.onClick.RemoveListener(ClosePanel);
+        }
+    }
+
+    private void ClosePanel()
+    {
+        UIManager.Instance.Hide(UIKey.EnumToString(_uIKeyEnumToClose));
+    }
+}
