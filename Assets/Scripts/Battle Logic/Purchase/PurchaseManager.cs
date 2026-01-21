e53b7ee7eb8b1c20878d11294e0123a4b362363a
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 18:44:15 +0900

    refactor: 초기화 순서 조절을 통한 의존성 정리
    - 각 매니저끼리 참조하지 않는 경우 생성자 주입, 매니저를 참조하게 되면 Initialize(), 명시적 작동 시작은 Activate(), 구독 등의 메모리 정리는 Deactivate()로 나누어 잠재적 의존성 문제 해결

--- Assets/Scripts/Battle Logic/Purchase/PurchaseManager.cs
+++ Assets/Scripts/Battle Logic/Purchase/PurchaseManager.cs
@@ -1,13 +1,22 @@
 ﻿public class PurchaseManager
 {
     private readonly PurchaseService _purchaseService;
-    private readonly ISaveMark _saveMark;
+    private ISaveMark _saveMark;
 
-    public PurchaseManager(PurchaseService PurchaseService, ISaveMark saveMark)
+    public PurchaseManager(PurchaseService PurchaseService)
     {
         _purchaseService = PurchaseService;
+        
+    }
+
+    public void Initialize(ISaveMark saveMark)
+    {
         _saveMark = saveMark;
     }
+
+    public void Activate() { /* no op*/ }
+    public void Deactivate() { /* no op*/ }
+
 
     public bool CanPay(in Cost cost) => _purchaseService.CanPay(cost);
 
