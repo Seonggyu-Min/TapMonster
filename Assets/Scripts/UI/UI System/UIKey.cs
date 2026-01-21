cdd0288212f3a7598725bcec8ed9f9bd14ec75bb
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-21 11:44:57 +0900

    feat: UIKey 추가

---
+++ Assets/Scripts/UI/UI System/UIKey.cs
@@ -0,0 +1,22 @@
+﻿public static class UIKey
+{
+    public const string SkillInventoryPanel = "Skill Inventory Panel";
+
+
+    public static string EnumToString(UIKeyEnum key)
+    {
+        return key switch
+        {
+            UIKeyEnum.SkillInventoryPanel => SkillInventoryPanel,
+
+
+
+            _ => string.Empty
+        };
+    }
+}
+
+public enum UIKeyEnum
+{
+    SkillInventoryPanel,
+}
