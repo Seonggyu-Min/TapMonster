a67ed01daa49ba199f23c3521f8727d50f3601a8
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 12:43:58 +0900

    feat: 스테이지 변경 이벤트 추가

--- Assets/Scripts/Battle Logic/Model/StageModel.cs
+++ Assets/Scripts/Battle Logic/Model/StageModel.cs
@@ -1,13 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class StageModel
 {
     private int _currentStage = -1;
+
+    public event Action<int> OnStageChanged;
 
     public int CurrentStage => _currentStage;
 
     public void SetStage(int stage)
     {
         _currentStage = Mathf.Clamp(stage, 1, int.MaxValue);
+        OnStageChanged?.Invoke(_currentStage);
     }
 }
