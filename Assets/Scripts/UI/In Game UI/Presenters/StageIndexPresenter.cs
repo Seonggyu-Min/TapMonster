18714a7d5f1d59235c062a472cf64728eb624cce
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 12:52:11 +0900

    feat: 현재 스테이지 표기용 View 및 Presenter 추가

---
+++ Assets/Scripts/UI/In Game UI/Presenters/StageIndexPresenter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class StageIndexPresenter : IDisposable
+{
+    private StageIndexView _stageIndexView;
+    private StageManager _stageManager;
+
+
+    public StageIndexPresenter(StageIndexView stageIndexView, StageManager stageManager)
+    {
+        _stageIndexView = stageIndexView;
+        _stageManager = stageManager;
+    }
+    public void Initialize() { /*no op*/ }
+    public void Activate()
+    {
+        _stageManager.OnStageChanged += SetText;
+    }
+    public void Dispose()
+    {
+        _stageManager.OnStageChanged -= SetText;
+    }
+
+    private void SetText(int stageIndex)
+    {
+        _stageIndexView.SetStage(stageIndex);
+    }
+}
---
+++ Assets/Scripts/UI/In Game UI/Views/StageIndexView.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class StageIndexView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _stageIndexText;
+
+
+    public void SetStage(int i)
+    {
+        _stageIndexText.text = $"stage: {i}";
+    }
+}
