a23e35aaef2933aed73c4d6c8277d9982300acdf
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 12:52:37 +0900

    feat: 추가된 UI Presenter 생성 및 주입 기능 추가

--- Assets/Scripts/UI/InGameUIComposer.cs
+++ Assets/Scripts/UI/InGameUIComposer.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameUIComposer : MonoBehaviour
 {
+    #region Fields
+
     [Header("Upgrade Panel Refs")]
     [SerializeField] private UpgradePanelView _upgradePanelView;
     
@@ -15,14 +18,43 @@ public class InGameUIComposer : MonoBehaviour
 
     [Header("Skill Slot Panel Refs")]
     [SerializeField] private SkillLoadoutItemView[] _skillLoadoutItemViews;
+    
+    [Header("Manual Input Refs")]
+    [SerializeField] private Button _manualAttackButton;
+
+    [Header("Monster HP UI Refs")]
+    [SerializeField] private MonsterHpView _monsterHpView;
+
+    [Header("Damage Text UI Refs")]
+    [SerializeField] private Transform _poolParent;              // 풀 보관용
+    [SerializeField] private RectTransform _spawnPoint;              // 스폰 포인트
+    [SerializeField] private DamageTextView _damageTextViewPrefab;
+
+    [Header("Stage Index UI Refs")]
+    [SerializeField] private StageIndexView _stageIndexView;
 
 
     private List<IDisposable> _disposables = new();
     private GameContext _gameContext;
     private GameConfigSO _gameConfigSO;
 
-    private SkillLoadoutPresenter _skillLoadoutPresenter;
+    // Spawners
+    private PoolHub _poolHub;
 
+
+    // Presenters
+    private UpgradePanelPresenter _upgradePanelPresenter;
+    private SkillPanelPresenter _skillPanelPresenter;
+    private SkillLoadoutPresenter _skillLoadoutPresenter;
+    private ManualInputPresenter _manualInputPresenter;
+    private MonsterHpUIPresenter _monsterHpUIPresenter;
+    private DamageUIPresenter _damageUIPresenter;
+    private StageIndexPresenter _stageIndexPresenter;
+
+    #endregion
+
+
+    #region Public Methods
 
     public void Init(GameContext gameContext, GameConfigSO gameConfigSO)
     {
@@ -34,43 +66,17 @@ public class InGameUIComposer : MonoBehaviour
     {
         DisposeAll();
 
-        // --- C# Presenters ---
-        var upgradePresenter = new UpgradePanelPresenter(
-            _upgradePanelView,
-            _gameConfigSO.UpgradeConfigSO.UpgradeDefs,
-            _gameContext.UpgradeManager,
-            _gameContext.WalletManager,
-            _gameContext.PurchaseManager
-        );
-        upgradePresenter.Initialize();
-        _disposables.Add(upgradePresenter);
+        ConstructSpawners();
 
-        var skillPresenter = new SkillPanelPresenter(
-            _skillPanelView,
-            _gameConfigSO.SkillConfigSO.SkillDefs,
-            _gameContext.SkillManager,
-            _gameContext.WalletManager,
-            _gameContext.PurchaseManager
-        );
-        skillPresenter.Initialize();
-        _disposables.Add(skillPresenter);
-
-        _skillLoadoutPresenter = new SkillLoadoutPresenter(
-            _gameContext.SkillManager,
-            _gameConfigSO.SkillConfigSO,
-            _skillLoadoutItemViews
-        );
-        _skillLoadoutPresenter.Initialize();
-        _disposables.Add(_skillLoadoutPresenter);
-
-
-        // --- Mono Behaviours ---
-        _skillUIOrchestrator.Init(
-            _gameConfigSO.SkillConfigSO,
-            _gameContext.SkillManager,
-            _gameContext.SaveLoadManager
-            );
+        ConstructPresenters();
+        InitializePresenters();
+        ActivatePresenters();
     }
+
+    #endregion
+
+
+    #region Unity Methods
 
     private void Update()
     {
@@ -82,6 +88,114 @@ public class InGameUIComposer : MonoBehaviour
         DisposeAll();
     }
 
+    #endregion
+
+
+    #region Private Methods
+
+    private void ConstructSpawners()
+    {
+        ObjectPool<DamageTextView> damagePool = new(
+            _poolParent,
+            _damageTextViewPrefab,
+            10
+            );
+        DamageTextSpawner damageTextSpawner = new(damagePool, _spawnPoint);
+
+
+
+
+        _poolHub = new()
+        {
+            DamageTextSpawner = damageTextSpawner,
+        };
+    }
+
+
+    private void ConstructPresenters()
+    {
+        _upgradePanelPresenter = new(
+            _upgradePanelView,
+            _gameConfigSO.UpgradeConfigSO.UpgradeDefs,
+            _gameContext.UpgradeManager,
+            _gameContext.WalletManager,
+            _gameContext.PurchaseManager
+            );
+        _disposables.Add(_upgradePanelPresenter);
+
+        _skillPanelPresenter = new(
+            _skillPanelView,
+            _gameConfigSO.SkillConfigSO.SkillDefs,
+            _gameContext.SkillManager,
+            _gameContext.WalletManager,
+            _gameContext.PurchaseManager
+            );
+        _disposables.Add(_skillPanelPresenter);
+
+        _skillLoadoutPresenter = new(
+            _gameContext.SkillManager,
+            _gameConfigSO.SkillConfigSO,
+            _skillLoadoutItemViews
+            );
+        _disposables.Add(_skillLoadoutPresenter);
+
+        _manualInputPresenter = new(
+            _manualAttackButton,
+            _gameContext.CombatManager
+            );
+        _disposables.Add(_manualInputPresenter);
+
+        _damageUIPresenter = new(
+            _gameContext.CombatManager,
+            _poolHub.DamageTextSpawner,
+            _spawnPoint
+            );
+        _disposables.Add(_damageUIPresenter);
+
+        _monsterHpUIPresenter = new(
+            _monsterHpView,
+            _gameContext.StageManager
+            );
+        _disposables.Add(_monsterHpUIPresenter);
+
+        _stageIndexPresenter = new(
+            _stageIndexView,
+            _gameContext.StageManager
+            );
+        _disposables.Add(_stageIndexPresenter);
+    }
+
+    private void InitializePresenters()
+    {
+        // C# Presenters
+        _upgradePanelPresenter.Initialize();
+        _skillPanelPresenter.Initialize();
+        _skillLoadoutPresenter.Initialize();
+        _manualInputPresenter.Initialize();
+        _damageUIPresenter.Initialize();
+        _monsterHpUIPresenter.Initialize();
+        _stageIndexPresenter.Initialize();
+
+        // Mono Behaviour Presenters
+        _skillUIOrchestrator.Initialize(
+            _gameConfigSO.SkillConfigSO,
+            _gameContext.SkillManager,
+            _gameContext.SaveLoadManager
+            );
+    }
+
+    private void ActivatePresenters()
+    {
+        _upgradePanelPresenter.Activate();
+        _skillPanelPresenter.Activate();
+        _skillLoadoutPresenter.Activate();
+        _manualInputPresenter.Activate();
+        _skillUIOrchestrator.Activate();
+        _damageUIPresenter.Activate();
+        _monsterHpUIPresenter.Activate();
+        _stageIndexPresenter.Activate();
+    }
+
     private void DisposeAll()
     {
         for (int i = 0; i < _disposables.Count; i++)
@@ -90,4 +204,6 @@ public class InGameUIComposer : MonoBehaviour
         }
         _disposables.Clear();
     }
+
+    #endregion
 }
