f9a09722d7eb76bc6cad57c416ad675931c11c8f
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 12:45:15 +0900

    feat: 스폰 로직 수정

--- Assets/Scripts/Battle Logic/Monster/MonsterSpawner.cs
+++ Assets/Scripts/Battle Logic/Monster/MonsterSpawner.cs
@@ -1,21 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MonsterSpawner : MonoBehaviour
 {
-    [SerializeField] private NormalMonster _monsterPrefab;
+    [SerializeField] private NormalMonster _normalPrefab;
+    [SerializeField] private NormalMonster _bossPrefab;
+    [SerializeField] private Transform _root;
 
     private NormalMonster _spawned;
 
-    
 
-    public void SpawnAndBind(GameContext ctx)
+    // TODO: 보스도 받게 바꿔야 됨
+    public NormalMonster Spawn(StageManager stageManager, bool resetHp)
     {
-        if (_spawned == null)
-            _spawned = Instantiate(_monsterPrefab);
+        NormalMonster prefab = stageManager.CurrentTargetType == TargetType.Boss
+            ? _bossPrefab : _normalPrefab;
 
-        ctx.StageManager.BindEnemy(_spawned);
-        ctx.StageManager.SpawnOrResetEnemy();
+        if (_spawned == null || _spawned.gameObject.name.StartsWith(prefab.name) == false)
+        {
+            if (_spawned != null) Destroy(_spawned.gameObject);
+            _spawned = Instantiate(prefab, _root);
+        }
+
+        stageManager.BindEnemyView(_spawned);
+        if (resetHp)
+        {
+            stageManager.SpawnOrResetEnemy();
+        }
+
+        return _spawned;
     }
 }
