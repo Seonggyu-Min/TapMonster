bcc1dc743c59e4739d30a237dd8610750f6ed47f
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 18:02:10 +0900

    feat: 공격 실패 처리 추가
    - 중복되는 로직 TryHitBase에서 실제 데미지 적용하도록 리팩토링
    - 공격 실패 처리 추가

--- Assets/Scripts/Battle Logic/Damage Flow/CombatManager.cs
+++ Assets/Scripts/Battle Logic/Damage Flow/CombatManager.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using UnityEngine;
 
 public interface ITargetProvider
 {
@@ -8,6 +7,9 @@ public interface ITargetProvider
 }
 
 
+/// <summary>
+/// 공격 input의 단일 진입점입니다.
+/// </summary>
 public class CombatManager
 {
     private readonly CombatService _combatService;
@@ -43,7 +45,6 @@ public class CombatManager
 
     public DamageResult TryManual()
     {
-        Debug.Log("TryManual triggered.");
         DamageRequest req = new(
             source: DamageSource.Manual,
             skillId: 0,
@@ -51,39 +52,41 @@ public class CombatManager
             targetType: _targetProvider.CurrentTargetType
             );
 
-        DamageResult r = _combatService.ResolveHit(
-            snapshot: _statManager.GetOrBuildSnapshot(),
-            req: req,
-            modifiers: _modifiers,
-            target: _targetProvider.CurrentTarget
-            );
-
-        OnHit?.Invoke(r);
-        return r;
+        return TryHitBase(req);
     }
 
     public DamageResult TryAuto()
     {
-        DamageRequest req = new DamageRequest(
+        DamageRequest req = new(
             source: DamageSource.Auto,
             skillId: 0,
             skillLevel: 0,
             targetType: _targetProvider.CurrentTargetType
             );
-
-        DamageResult r = _combatService.ResolveHit(
-            snapshot: _statManager.GetOrBuildSnapshot(),
-            req: req,
-            modifiers: _modifiers,
-            target: _targetProvider.CurrentTarget
-            );
-
-        OnHit?.Invoke(r);
-        return r;
+        return TryHitBase(req);
     }
 
     public DamageResult TrySkill(int skillId)
     {
+        // 1. 타겟 확보, 스킬 사용 먼저 해봐야 돼서 여기서 먼저 처리
+        IDamageable target = _targetProvider.CurrentTarget;
+        if (target == null)
+        {
+            return DamageResultFactory.FailNoTarget(DamageSource.Skill, skillId);
+        }
+        if (target.IsDead)
+        {
+            return DamageResultFactory.FailTargetDead(DamageSource.Skill, skillId);
+        }
+
+        // 2. 사용 가능 체크 / 성공 시 쿨타임 시작
+        SkillUseResult use = _skillManager.TryUseSkill(skillId);
+        if (use != SkillUseResult.Success)
+        {
+            return DamageResultFactory.FailSkillBlocked(DamageSource.Skill, skillId, use);
+        }
+
+        // 3. 스킬 정의 기반으로 DamageRequest 구성
         _skillConfigSO.TryGet(skillId, out var def);
 
         float perLevel = 0f;
@@ -96,20 +99,52 @@ public class CombatManager
             perLevel = (float)def.PassiveValuePerLevel;
         }
 
-        DamageRequest req = new DamageRequest(
+        DamageRequest req = new(
             source: DamageSource.Skill,
             skillId: skillId,
             skillLevel: _skillManager.GetLevel(skillId),
             targetType: _targetProvider.CurrentTargetType,
             skillMulPerLevel: perLevel,
-            canCriticalOverride: true);
+            canCriticalOverride: true
+        );
 
-        DamageResult r = _combatService.ResolveHit(
+        return TryHitBase(req, target);
+    }
+
+
+    private DamageResult TryHitBase(DamageRequest req, IDamageable target = null)
+    {
+        target ??= _targetProvider.CurrentTarget;
+
+        if (target == null)
+        {
+            return DamageResultFactory.FailNoTarget(req.Source, req.SkillId);
+        }
+        if (target.IsDead)
+        {
+            return DamageResultFactory.FailTargetDead(req.Source, req.SkillId);
+        }
+        
+        // 계산
+        DamageResultPreview preview = _combatService.ResolveDamageOnly(
             snapshot: _statManager.GetOrBuildSnapshot(),
             req: req,
-            modifiers: _modifiers,
-            target: _targetProvider.CurrentTarget
-            );
+            modifiers: _modifiers
+        );
+
+        // 적용
+        BigNumber applied = target.ApplyDamage(preview.CalculatedDamage);
+        bool died = target.IsDead;
+
+        // 최종 결과
+        DamageResult r = DamageResultFactory.Success(
+            req.Source,
+            req.SkillId,
+            preview.IsCritical,
+            preview.CalculatedDamage,
+            applied,
+            died
+        );
 
         OnHit?.Invoke(r);
         return r;
