6da476e0425ec8db0329a5847f20b0fc766828f3
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 12:52:56 +0900

    feat: 부모 및 자식에서 컴포넌트 바인딩 기능 추가

--- Assets/Scripts/Utils/ComponentExtensions.cs
+++ Assets/Scripts/Utils/ComponentExtensions.cs
@@ -15,4 +15,38 @@ public static class ComponentExtensions
         owner.PrintLog($"{typeof(T).Name} 컴포넌트를 찾을 수 없습니다.", category, logType);
         return false;
     }
+
+    public static bool TryBindComponentInChildren<T>(
+        this Component owner,
+        ref T field,
+        LogCategory category = LogCategory.UI,
+        bool includeInactive = true,
+        LogType logType = LogType.Error)
+        where T : Component
+    {
+        if (field != null) return true;
+
+        field = owner.GetComponentInChildren<T>(includeInactive);
+        if (field != null) return true;
+
+        owner.PrintLog($"{typeof(T).Name} (in children) 컴포넌트를 찾을 수 없습니다.", category, logType);
+        return false;
+    }
+
+    public static bool TryBindComponentInParent<T>(
+        this Component owner,
+        ref T field,
+        LogCategory category = LogCategory.UI,
+        bool includeInactive = true,
+        LogType logType = LogType.Error)
+        where T : Component
+    {
+        if (field != null) return true;
+
+        field = owner.GetComponentInParent<T>(includeInactive);
+        if (field != null) return true;
+
+        owner.PrintLog($"{typeof(T).Name} (in parent) 컴포넌트를 찾을 수 없습니다.", category, logType);
+        return false;
+    }
 }
