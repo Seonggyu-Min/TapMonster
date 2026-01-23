2312db8eefaaecae0e9beb8e6a222e535287c584
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 12:43:21 +0900

    refactor: InputManager에서 이름 변경 및 직접 전투 처리로 변경
    - InputManager에서 ManualInputPresenter로 클래스 이름 변경
    - GameCoordinator로 구독 기능 및 전투 처리를 넘기지 않고 직접 처리하도록 변경

---
+++ Assets/Scripts/Battle Logic/Input Controller/ManualInputPresenter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManualInputPresenter : IDisposable
+{
+    private readonly Button _manualAttackButton;
+    private readonly CombatManager _combatManager;
+
+    private bool _active;
+    private const LogCategory CurrentCategory = LogCategory.UI;
+
+    public ManualInputPresenter(Button manualAttackButton, CombatManager combatManager)
+    {
+        _manualAttackButton = manualAttackButton;
+        _combatManager = combatManager;
+    }
+    public void Initialize() { /*no op*/ }
+    public void Activate()
+    {
+        if (_active) return;
+        _active = true;
+
+        if (_manualAttackButton != null)
+        {
+            _manualAttackButton.onClick.AddListener(OnClick);
+        }
+    }
+
+    public void Dispose()
+    {
+        if (!_active) return;
+        _active = false;
+
+        if (_manualAttackButton != null)
+        {
+            _manualAttackButton.onClick.RemoveListener(OnClick);
+        }
+    }
+
+    private void OnClick()
+    {
+        if (_combatManager == null)
+        {
+            this.PrintLog("_combatManager가 null입니다.", CurrentCategory, LogType.Error);
+            return;
+        }
+
+        _combatManager.TryManual();
+        this.PrintLog("Manual attack", CurrentCategory);
+    }
+}
