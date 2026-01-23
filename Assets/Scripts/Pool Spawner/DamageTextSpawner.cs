58aff5a03d946a4acb544822a0f4963da615afa4
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 12:50:46 +0900

    feat: 데미지 텍스트 풀링 스폰 기능 추가

---
+++ Assets/Scripts/Pool Spawner/DamageTextSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DamageTextSpawner
+{
+    private readonly ObjectPool<DamageTextView> _pool;
+    private readonly RectTransform _root;
+
+    public DamageTextSpawner(ObjectPool<DamageTextView> pool, RectTransform root)
+    {
+        _pool = pool;
+        _root = root;
+    }
+
+    public void Spawn(string text, RectTransform spawnPoint)
+    {
+        if (spawnPoint == null) return;
+
+        DamageTextView view = _pool.PopPool(_root);
+        Vector2 anchoredPos = (Vector2)_root.InverseTransformPoint(spawnPoint.position);
+        view.Show(text, anchoredPos);
+    }
+}
