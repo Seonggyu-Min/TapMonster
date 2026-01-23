c44dcebf968518322249a5b0443e9be8103e4498
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 18:08:15 +0900

    feat: 소지한 골드 표기용 View 및 Presenter 추가

---
+++ Assets/Scripts/UI/In Game UI/Presenters/WalletPresenter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class WalletPresenter : IDisposable
+{
+    private WalletManager _walletManager;
+    private WalletView _walletView;
+
+    private bool _activated;
+
+    private LogCategory CurrentCategory = LogCategory.UI;
+
+
+    public WalletPresenter(WalletView walletView, WalletManager walletManager)
+    {
+        _walletView = walletView;
+        _walletManager = walletManager;
+    }
+    public void Initialize() { /*no op*/ }
+    public void Activate()
+    {
+        if (_activated) return;
+        _activated = true;
+
+        _walletManager.OnCurrencyChanged += OnCurrencyChanged;
+    }
+    public void Dispose()
+    {
+        if (!_activated) return;
+        _activated = false;
+
+        _walletManager.OnCurrencyChanged -= OnCurrencyChanged;
+    }
+
+    private void OnCurrencyChanged(CurrencyId currency, BigNumber amount)
+    {
+        if (currency == CurrencyId.Gold)
+        {
+            _walletView.SetGold(amount);
+        }
+        else
+        {
+            // 추가 재화 생기면 처리
+            this.PrintLog($"처리되지 않은 재화 타입입니다: {currency}", CurrentCategory, LogType.Warning);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/In Game UI/Views/WalletView.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class WalletView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _walletText;
+
+    public void SetGold(BigNumber amount)
+    {
+        _walletText.text = $"Gold: {BigNumberFormatter.ToString(amount)}";
+    }
+}
