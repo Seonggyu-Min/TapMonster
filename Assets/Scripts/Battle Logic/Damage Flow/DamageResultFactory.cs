7acef1fc56966f07b120cfde4edab1e8a57edf04
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 18:04:14 +0900

    feat: DamageResult를 가볍게 생성하기 위한 팩토리 추가
    - 많은 생성자 매개변수를 가지면서 팩토리 추가

---
+++ Assets/Scripts/Battle Logic/Damage Flow/DamageResultFactory.cs
@@ -0,0 +1,78 @@
+﻿public static class DamageResultFactory
+{
+    public static DamageResult FailNoTarget(
+        DamageSource source,
+        int skillId
+        )
+    {
+        return new DamageResult(
+            isSuccess: false,
+            failReason: DamageFailReason.NoTarget,
+            skillFailReason: null,
+            source: source,
+            skillId: skillId,
+            isCritical: false,
+            calculatedDamage: BigNumber.Zero,
+            appliedDamage: BigNumber.Zero,
+            targetDied: false
+            );
+    }
+
+    public static DamageResult FailSkillBlocked(
+        DamageSource source,
+        int skillId,
+        SkillUseResult reason
+        )
+    {
+        return new DamageResult(
+            isSuccess: false,
+            failReason: DamageFailReason.SkillBlocked,
+            skillFailReason: reason,
+            source: source,
+            skillId: skillId,
+            isCritical: false,
+            calculatedDamage: BigNumber.Zero,
+            appliedDamage: BigNumber.Zero,
+            targetDied: false
+            );
+    }
+
+    public static DamageResult FailTargetDead(
+        DamageSource source,
+        int skillId
+        )
+    {
+        return new DamageResult(
+            isSuccess: false,
+            failReason: DamageFailReason.TargetDead,
+            skillFailReason: null,
+            source: source,
+            skillId: skillId,
+            isCritical: false,
+            calculatedDamage: BigNumber.Zero,
+            appliedDamage: BigNumber.Zero,
+            targetDied: true
+            );
+    }
+
+    public static DamageResult Success(
+        DamageSource source,
+        int skillId,
+        bool isCritical,
+        BigNumber calculated,
+        BigNumber applied,
+        bool targetDied
+        )
+    {
+        return new DamageResult(
+            isSuccess: true,
+            failReason: DamageFailReason.None,
+            skillFailReason: null,
+            source: source,
+            skillId: skillId,
+            isCritical: isCritical,
+            calculatedDamage: calculated,
+            appliedDamage: applied,
+            targetDied: targetDied);
+    }
+}
