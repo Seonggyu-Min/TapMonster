055ceb003df91baa82248176264a0cf961357782
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-23 12:49:16 +0900

    feat: 몬스터 최대 체력 및 보스 여부 기능 추가

---
+++ Assets/Scripts/Battle Logic/Stage/StageMaxHpService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class StageMaxHpService
+{
+    private readonly StageConfigSO _stageConfigSO;
+
+    private IDamageable _currentEnemy;
+    public event Action OnMonsterDefeated;
+
+    public IDamageable CurrentEnemy => _currentEnemy;
+
+
+    public StageMaxHpService(StageConfigSO stageConfigSO)
+    {
+        _stageConfigSO = stageConfigSO;
+    }
+
+    public BigNumber GetMonsterMaxHp(int stage) => _stageConfigSO.GetMaxHp(stage);
+    public bool IsBossStage(int stage) => _stageConfigSO.IsBossStage(stage);
+}
