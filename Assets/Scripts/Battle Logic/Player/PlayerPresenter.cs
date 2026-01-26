cdb905b6e8258fe0dd3188a46c03aec155fd4a95
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-26 12:10:01 +0900

    feat: 수동공격 애니메이션 추가

--- Assets/Scripts/Battle Logic/Player/PlayerPresenter.cs
+++ Assets/Scripts/Battle Logic/Player/PlayerPresenter.cs
@@ -1,36 +1,58 @@
 ﻿using System;
-using UnityEngine;
 
 public class PlayerPresenter : IDisposable
 {
     private PlayerView _playerView;
+    private CombatManager _combatManager;
     private SkillManager _skillManager;
-    private ManualInputPresenter _inputManager;
-    
+    private ManualAttackConfigSO _manualAttackConfigSO;
+    private SkillConfigSO _skillConfigSO;
+
+    private bool _activated;
 
     public PlayerPresenter(
         PlayerView playerView,
+        CombatManager combatManager,
         SkillManager skillManager,
-        ManualInputPresenter inputManager
+        ManualAttackConfigSO manualAttackConfigSO,
+        SkillConfigSO skillConfigSO
         )
     {
         _playerView = playerView;
+        _combatManager = combatManager;
         _skillManager = skillManager;
-        _inputManager = inputManager;
+        _manualAttackConfigSO = manualAttackConfigSO;
+        _skillConfigSO = skillConfigSO;
     }
-
-
-    public void Initialize()
+    public void Initialize() { /*no op*/ }
+    public void Activate()
     {
-        //_inputManager.OnManualAttack += HandleManualAttack;
-        //_skillManager.OnSkillUsed += HandleSkillUsed;
+        if (_activated) return;
+        _activated = true;
 
+        _skillManager.OnSkillUsed += HandleSkillAnimation;
+        _combatManager.OnHit += HandleManualAnimation;
     }
-
-
-
     public void Dispose()
     {
-        
+        if (!_activated) return;
+        _activated = false;
+
+        _skillManager.OnSkillUsed -= HandleSkillAnimation;
+        _combatManager.OnHit -= HandleManualAnimation;
+    }
+
+    private void HandleManualAnimation(DamageResult damageResult)
+    {
+        if (damageResult.Source != DamageSource.Manual) return;
+        var currentKey = _manualAttackConfigSO.AttackAnimationKey;
+        _playerView.PlayAnimation(currentKey);
+    }
+
+    private void HandleSkillAnimation(SkillUseEvent skillUseEvent)
+    {
+        _skillConfigSO.TryGet(skillUseEvent.SkillId, out var def);
+        var currentKey = def.AnimationKey;
+        _playerView.PlayAnimation(currentKey);
     }
 }
--- Assets/Scripts/Battle Logic/Player/PlayerView.cs
+++ Assets/Scripts/Battle Logic/Player/PlayerView.cs
@@ -3,31 +3,32 @@
 public class PlayerView : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    
 
     private const LogCategory CurrentCategory = LogCategory.Animation;
 
 
-    // 수동 공격 연출
-    public void PlayManualAttackAnimation(AnimationKey key)
-    {
-        Play(key);
-    }
+    //// 수동 공격 연출
+    //public void PlayManualAttackAnimation(AnimationKey key)
+    //{
+    //    Play(key);
+    //}
 
-    // 스킬 연출
-    public void PlaySkillAnimation(AnimationKey key)
-    {
-        Play(key);
-    }
+    //// 스킬 연출
+    //public void PlaySkillAnimation(AnimationKey key)
+    //{
+    //    Play(key);
+    //}
 
-    private void Play(AnimationKey key)
+    public void PlayAnimation(AnimationKey key)
     {
-        int hash = AnimationHashes.Get(AnimationSubject.Player, key);
+        int hash = AnimationHashes.Get(key);
         if (hash == 0)
         {
             this.PrintLog($"애니메이션을 찾을 수 없습니다 key: {key}", CurrentCategory, LogType.Warning);
             return;
         }
 
-        _animator.SetTrigger(hash);
+        _animator.Play(hash, 0, 0f);
     }
 }
