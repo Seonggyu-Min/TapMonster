6caba2e0b265f45846dbc19c8717230ecd03e2be
Seonggyu-Min <minsunggu705@gmail.com>
2026-01-26 14:19:24 +0900

    refactor: 더 이상 사용하지 않는 개별 애니메이션 재생 메서드 삭제

--- Assets/Scripts/Battle Logic/Player/PlayerView.cs
+++ Assets/Scripts/Battle Logic/Player/PlayerView.cs
@@ -8,18 +8,6 @@ public class PlayerView : MonoBehaviour
     private const LogCategory CurrentCategory = LogCategory.Animation;
 
 
-    //// 수동 공격 연출
-    //public void PlayManualAttackAnimation(AnimationKey key)
-    //{
-    //    Play(key);
-    //}
-
-    //// 스킬 연출
-    //public void PlaySkillAnimation(AnimationKey key)
-    //{
-    //    Play(key);
-    //}
-
     public void PlayAnimation(AnimationKey key)
     {
         int hash = AnimationHashes.Get(key);
